42f84a25ab271f7ddabbd23589695361d238e8cb
jmason <jmason@ebi.ac.uk>
2017-03-16 11:52:09 +0000

    Change username and password to be examples

--- web/src/main/java/org/pdxfinder/Application.java
+++ web/src/main/java/org/pdxfinder/Application.java
@@ -2,8 +2,10 @@ package org.pdxfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class Application {
 
     public static void main(String[] args) {
