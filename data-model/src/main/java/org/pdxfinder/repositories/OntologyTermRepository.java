0cbcd00dea429d938ae663193c325c37201f3cd5
csaba <csaba@ebi.ac.uk>
2017-06-08 14:02:11 +0100

    Add missing annotation

--- data-model/src/main/java/org/pdxfinder/repositories/OntologyTermRepository.java
+++ data-model/src/main/java/org/pdxfinder/repositories/OntologyTermRepository.java
@@ -3,11 +3,13 @@ package org.pdxfinder.repositories;
 import org.pdxfinder.dao.OntologyTerm;
 import org.pdxfinder.dao.Sample;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by csaba on 07/06/2017.
  */
-public interface OntologyTermRepository extends PagingAndSortingRepository<Sample, Long> {
+@Repository
+public interface OntologyTermRepository extends PagingAndSortingRepository<OntologyTerm, Long> {
 
     OntologyTerm findById();
 
