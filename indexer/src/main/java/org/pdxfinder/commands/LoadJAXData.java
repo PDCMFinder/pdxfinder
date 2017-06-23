869215c88ce0af643a929a9cece3d09cf9ad8026
Steven Neuhauser <steven.neuhauser@jax.org>
2017-06-23 14:53:49 -0400

    Cleaned up code

--- indexer/src/main/java/org/pdxfinder/commands/LoadJAXData.java
+++ indexer/src/main/java/org/pdxfinder/commands/LoadJAXData.java
@@ -42,10 +42,10 @@ public class LoadJAXData implements CommandLineRunner {
     private final static String NSG_BS_NAME = "NSG (NOD scid gamma)";
     private final static String NSG_BS_SYMBOL = "NOD.Cg-Prkdc<sup>scid</sup> Il2rg<sup>tm1Wjl</sup>/SzJ"; //yay HTML in name
     private final static String NSG_BS_URL = "http://jax.org/strain/005557";
+    private final static String HISTOLOGY_NOTE = "Pathologist assessment of patient tumor and pdx model tumor histology slides.";
 
     // for now all samples are of tumor tissue
     private final static Boolean NORMAL_TISSUE_FALSE = false;
-
 
     private BackgroundStrain nsgBS;
     private ExternalDataSource jaxDS;
@@ -66,13 +66,13 @@ public class LoadJAXData implements CommandLineRunner {
 
     @Value("${jaxpdx.variation.url}")
     private String variationURL;
-    
+
     @Value("${jaxpdx.histology.url}")
     private String histologyURL;
 
     @Value("${jaxpdx.variation.max}")
     private int maxVariations;
-    
+
     HashMap<String, String> passageMap = null;
     HashMap<String, String> histologyMap = null;
 
@@ -92,11 +92,9 @@ public class LoadJAXData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        log.info(args[0]);
-
         if ("loadJAX".equals(args[0]) || "-loadJAX".equals(args[0])) {
 
-            log.info("Loading JAX PDX data.");
+            System.out.println("Loading JAX PDX data.");
             try {
                 cmd = parser.parse(options, args);
 
@@ -121,44 +119,12 @@ public class LoadJAXData implements CommandLineRunner {
             } else {
                 log.error("No jaxpdx.file or jaxpdx.url provided in properties");
             }
+        } else {
+            System.out.println("'" + args[0] + "' is not a recognized argument. Try 'loadJAX' to load JAX data.");
         }
-
     }
 
-    private String parseURL(String urlStr) {
-        StringBuilder sb = new StringBuilder();
-
-        try {
-            URL url = new URL(urlStr);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(url.openStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                sb.append(inputLine);
-            }
-            in.close();
-        } catch (Exception e) {
-            log.error("Unable to read from URL " + urlStr, e);
-        }
-        return sb.toString();
-    }
-
-    private String parseFile(String path) {
-
-        StringBuilder sb = new StringBuilder();
-
-        try {
-            Stream<String> stream = Files.lines(Paths.get(path));
-
-            Iterator itr = stream.iterator();
-            while (itr.hasNext()) {
-                sb.append(itr.next());
-            }
-        } catch (Exception e) {
-            log.error("Failed to load file " + path, e);
-        }
-        return sb.toString();
-    }
+    
 
     //JSON Fields {"Model ID","Gender","Age","Race","Ethnicity","Specimen Site","Primary Site","Initial Diagnosis","Clinical Diagnosis",
     //  "Tumor Type","Grades","Tumor Stage","Markers","Sample Type","Strain","Mouse Sex","Engraftment Site"};
@@ -166,7 +132,6 @@ public class LoadJAXData implements CommandLineRunner {
 
         jaxDS = loaderUtils.getExternalDataSource(JAX_DATASOURCE_ABBREVIATION, JAX_DATASOURCE_NAME, JAX_DATASOURCE_DESCRIPTION);
         nsgBS = loaderUtils.getBackgroundStrain(NSG_BS_SYMBOL, NSG_BS_NAME, NSG_BS_NAME, NSG_BS_URL);
-
 
         try {
             JSONObject job = new JSONObject(json);
@@ -180,7 +145,7 @@ public class LoadJAXData implements CommandLineRunner {
             }
 
         } catch (Exception e) {
-            log.error("", e);
+            log.error("Error getting JAX PDX models", e);
 
         }
     }
@@ -188,33 +153,32 @@ public class LoadJAXData implements CommandLineRunner {
     @Transactional
     private void createGraphObjects(JSONObject j) throws Exception {
         String id = j.getString("Model ID");
-        
+
         histologyMap = getHistologyImageMap(id);
-        
+
         // the preference is for clinical diagnosis but if not available use initial diagnosis
         String diagnosis = j.getString("Clinical Diagnosis");
-        if(diagnosis.trim().length()==0 || "Not Specified".equals(diagnosis)){
+        if (diagnosis.trim().length() == 0 || "Not Specified".equals(diagnosis)) {
             diagnosis = j.getString("Initial Diagnosis");
-            
+
         }
-        
+
         String classification = j.getString("Tumor Stage") + "/" + j.getString("Grades");
 
         PatientSnapshot pSnap = loaderUtils.getPatientSnapshot(j.getString("Model ID"), j.getString("Gender"),
                 j.getString("Race"), j.getString("Ethnicity"), j.getString("Age"), jaxDS);
 
-        Sample sample = loaderUtils.getSample(j.getString("Model ID"), j.getString("Tumor Type"),diagnosis ,
+        Sample sample = loaderUtils.getSample(j.getString("Model ID"), j.getString("Tumor Type"), diagnosis,
                 j.getString("Primary Site"), j.getString("Specimen Site"), classification, NORMAL_TISSUE_FALSE, jaxDS);
-        
-        if(histologyMap.containsKey("Patient")){
+
+        if (histologyMap.containsKey("Patient")) {
             Histology histology = new Histology();
             Image image = new Image(histologyMap.get("Patient"));
             histology.addImage(image);
             sample.addHistology(histology);
-            
+
         }
 
-        
         JSONArray markers = j.getJSONArray("Markers");
         HashMap<String, Set<MarkerAssociation>> markerMap = new HashMap<>();
         for (int mIndex = 0; mIndex < markers.length(); mIndex++) {
@@ -251,7 +215,7 @@ public class LoadJAXData implements CommandLineRunner {
 
         // For the moment, all JAX models are assumed to have been validated using Histological assessment by a pathologist
         // TODO: verify this is the case
-        QualityAssurance qa = new QualityAssurance("Histology", "Pathologist assessment of patient tumor and pdx model tumor histology slides.", ValidationTechniques.VALIDATION);
+        QualityAssurance qa = new QualityAssurance("Histology", HISTOLOGY_NOTE, ValidationTechniques.VALIDATION);
         loaderUtils.saveQualityAssurance(qa);
 
         ModelCreation mc = loaderUtils.createModelCreation(id, j.getString("Engraftment Site"), j.getString("Sample Type"), sample, nsgBS, qa);
@@ -261,34 +225,28 @@ public class LoadJAXData implements CommandLineRunner {
     }
 
 
- 
-    
-    
-
-    //JSON fields: "model id","sample","gene symbol","platform","amino acid change","passage num"
-    // create validation with MC
-    // attach validation to passage
-    // attach passage to model
-    // expliclty exclude patient samples for now
-    // for each set of variation data
-    // map <sample,map<tech,marker>>
+    /*
+    Use the modelID in the modelCreation object to get model specific variation data
+    This is a set of makers with marker association details
+    Since we are creating samples here attach any histology images to the sample based on passage #
+     */
     private void loadVariationData(ModelCreation modelCreation) {
 
-        if (maxVariations == 0) return;
+        if (maxVariations == 0) {
+            return;
+        }
 
         try {
-            // marker assocations keyed to technology/platform
 
             passageMap = new HashMap<>();
 
             HashMap<String, HashMap<String, Set<MarkerAssociation>>> sampleMap = new HashMap<>();
             HashMap<String, Set<MarkerAssociation>> markerMap = new HashMap<>();
 
-
             JSONObject job = new JSONObject(parseURL(this.variationURL + modelCreation.getSourcePdxId()));
             JSONArray jarray = job.getJSONArray("variation");
-            String sample, symbol, technology, aaChange, chromosome,seqPosition,refAllele,consequence,rsVariants,readDepth,alleleFrequency;
-            System.out.println(jarray.length() + " gene variants for model " + modelCreation.getSourcePdxId());
+            String sample, symbol, technology, aaChange, chromosome, seqPosition, refAllele, consequence, rsVariants, readDepth, alleleFrequency;
+            log.info(jarray.length() + " gene variants for model " + modelCreation.getSourcePdxId());
 
             // configure the maximum variations to load in properties file
             // loading them all will take a while (hour?)
@@ -298,12 +256,11 @@ public class LoadJAXData implements CommandLineRunner {
             }
             for (int i = 0; i < stop;) {
                 JSONObject j = jarray.getJSONObject(i);
+
                 sample = j.getString("sample");
                 symbol = j.getString("gene symbol");
                 aaChange = j.getString("amino acid change");
                 technology = j.getString("platform");
-                passageMap.put(sample, j.getString("passage num"));
-                
                 chromosome = j.getString("chromosome");
                 seqPosition = j.getString("seq position");
                 refAllele = j.getString("ref allele");
@@ -312,10 +269,12 @@ public class LoadJAXData implements CommandLineRunner {
                 readDepth = j.getString("read depth");
                 alleleFrequency = j.getString("allele frequency");
 
+                passageMap.put(sample, j.getString("passage num"));
+
+                // since there are 8 fields assume all MAs are unique
+                // create a new one rather than look for exisitng one
                 MarkerAssociation ma = new MarkerAssociation();
-                Marker marker = loaderUtils.getMarker(symbol);
-                ma.setMarker(marker);
-                
+
                 ma.setAminoAcidChange(aaChange);
                 ma.setConsequence(consequence);
                 ma.setAlleleFrequency(alleleFrequency);
@@ -324,13 +283,17 @@ public class LoadJAXData implements CommandLineRunner {
                 ma.setRefAllele(refAllele);
                 ma.setRsVariants(rsVariants);
                 ma.setSeqPosition(seqPosition);
+                ma.setReadDepth(readDepth);
+
+                Marker marker = loaderUtils.getMarker(symbol);
+                ma.setMarker(marker);
 
                 markerMap = sampleMap.get(sample);
                 if (markerMap == null) {
                     markerMap = new HashMap<>();
                 }
 
-                // make a map of markerAssociationCollections keyed to technology
+                // make a map of markerAssociation collections keyed to technology
                 if (markerMap.containsKey(technology)) {
                     markerMap.get(technology).add(ma);
                 } else {
@@ -345,7 +308,7 @@ public class LoadJAXData implements CommandLineRunner {
                     System.out.println("loaded " + i + " markers");
                 }
             }
-            System.out.println("loaded " + stop + " markers");
+            System.out.println("loaded " + stop + " markers for " + modelCreation.getSourcePdxId());
 
             for (String sampleKey : sampleMap.keySet()) {
 
@@ -361,24 +324,22 @@ public class LoadJAXData implements CommandLineRunner {
 
                 }
 
-                
                 PdxPassage pdxPassage = new PdxPassage(modelCreation, passage);
-               
+
                 Specimen specimen = loaderUtils.getSpecimen(sampleKey);
                 specimen.setMolecularCharacterizations(mcs);
                 specimen.setPdxPassage(pdxPassage);
-                
-                if(histologyMap.containsKey(pdxPassage)){
+
+                if (histologyMap.containsKey(pdxPassage)) {
                     Histology histology = new Histology();
                     Image image = new Image(histologyMap.get(pdxPassage));
                     histology.addImage(image);
                     specimen.addHistology(histology);
 
                 }
-                
-                
+
                 pdxPassage.setModelCreation(modelCreation);
-                
+
                 loaderUtils.savePdxPassage(pdxPassage);
                 loaderUtils.saveSpecimen(specimen);
                 System.out.println("saved passage " + passage + " for model " + modelCreation.getSourcePdxId() + " from sample " + sampleKey);
@@ -400,37 +361,76 @@ public class LoadJAXData implements CommandLineRunner {
         return p;
 
     }
-    // we are loosing information here just getting PT or passage from description but for now its ok?
-    private HashMap<String,String> getHistologyImageMap(String id){
-        HashMap<String,String> map = new HashMap<>();
-        try{
+
+    /*
+    For a given model return a map of passage # or "Patient" -> histology image URL
+     */
+    private HashMap<String, String> getHistologyImageMap(String id) {
+        HashMap<String, String> map = new HashMap<>();
+        try {
             JSONObject job = new JSONObject(parseURL(this.histologyURL + id));
-                JSONArray jarray = job.getJSONObject("pdxHistology").getJSONArray("Graphics");
-                
-                for(int i = 0; i < jarray.length(); i++){
-                    job = jarray.getJSONObject(i);
-                    String desc = job.getString("Description");
-                    String url = job.getString("URL");
-                    if(desc.startsWith("Patient") || desc.startsWith("Primary")){
-                        map.put("Patient",url);
-                    }else{
-                        String[] parts = desc.split(" ");
-                        if(parts[0].startsWith("P")){
-                            try{
-                                String passage = new Integer(parts[0].replace("P", "")).toString();
-                                map.put(passage,url);
-                            }catch(Exception e){
-                                log.info("Can't extract passage from description "+desc);
-                            }
+            JSONArray jarray = job.getJSONObject("pdxHistology").getJSONArray("Graphics");
+
+            for (int i = 0; i < jarray.length(); i++) {
+                job = jarray.getJSONObject(i);
+                String desc = job.getString("Description");
+                String url = job.getString("URL");
+                if (desc.startsWith("Patient") || desc.startsWith("Primary")) {
+                    map.put("Patient", url);
+                } else {
+                    String[] parts = desc.split(" ");
+                    if (parts[0].startsWith("P")) {
+                        try {
+                            String passage = new Integer(parts[0].replace("P", "")).toString();
+                            map.put(passage, url);
+                        } catch (Exception e) {
+                            log.info("Can't extract passage from description " + desc);
                         }
                     }
                 }
-
-            }catch(Exception e){
-                log.error("Error getting histology for model "+id,e);
             }
 
+        } catch (Exception e) {
+            log.error("Error getting histology for model " + id, e);
+        }
+
         return map;
+    }
+    
+    
+    private String parseURL(String urlStr) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            URL url = new URL(urlStr);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            in.close();
+        } catch (Exception e) {
+            log.error("Unable to read from URL " + urlStr, e);
+        }
+        return sb.toString();
+    }
+
+    private String parseFile(String path) {
+
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            Stream<String> stream = Files.lines(Paths.get(path));
+
+            Iterator itr = stream.iterator();
+            while (itr.hasNext()) {
+                sb.append(itr.next());
+            }
+        } catch (Exception e) {
+            log.error("Failed to load file " + path, e);
+        }
+        return sb.toString();
     }
 
 }
