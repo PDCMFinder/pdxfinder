4ad6730371e3b6361979225027ffe0a642eefcf9
jmason <jmason@ebi.ac.uk>
2017-08-11 11:32:52 +0100

    make setup method public, include embedded graph driver

--- data-services/src/test/java/org/pdxfinder/services/SearchServiceTest.java
+++ data-services/src/test/java/org/pdxfinder/services/SearchServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.pdxfinder.BaseTest;
 import org.pdxfinder.repositories.SampleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.util.Assert;
 
@@ -12,22 +14,28 @@ import org.springframework.util.Assert;
  */
 public class SearchServiceTest extends BaseTest {
 
+    private final static Logger log = LoggerFactory.getLogger(SearchServiceTest.class);
+
     @MockBean
     private SampleRepository sampleRepository;
 
-
     @Before
-    private void setup() {
+    public void setup() {
         // Preload the base data in the graph for testing the service
-
+        log.info("Set up");
     }
 
     @Test
     public void searchForBreastCancerModels() throws Exception {
 
+        log.info("Testing mock sample repository");
         Assert.notNull(sampleRepository, "Sample repo is null");
 
     }
 
+    @Test
+    public void anotherTest() {
+        log.info("This is another test");
+    }
 
 }
