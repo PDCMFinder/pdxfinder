89686c934aa19ce6bc5a72a654f6a7d653a1e4c4
csaba <csaba@ebi.ac.uk>
2017-08-21 15:58:39 +0100

    initial commit

---
+++ indexer/src/main/java/org/pdxfinder/commands/ValidateGeneSymbols.java
@@ -0,0 +1,291 @@
+package org.pdxfinder.commands;
+
+import org.neo4j.ogm.json.JSONArray;
+import org.neo4j.ogm.json.JSONException;
+import org.neo4j.ogm.json.JSONObject;
+import org.pdxfinder.accessionidtatamodel.AccessionData;
+import org.pdxfinder.dao.Marker;
+import org.pdxfinder.dao.ModelCreation;
+import org.pdxfinder.utilities.LoaderUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+import java.util.*;
+
+/**
+ * Created by csaba on 21/08/2017.
+ */
+@Component
+@Order(value = Ordered.LOWEST_PRECEDENCE)
+public class ValidateGeneSymbols implements CommandLineRunner{
+
+
+    private static final String DATA_FILE_URL = "http://www.genenames.org/cgi-bin/download?col=gd_hgnc_id&col=gd_app_sym&col=gd_prev_sym&col=gd_aliases&col=md_eg_id&col=md_ensembl_id&status=Approved&status_opt=2&where=&order_by=gd_app_sym_sort&format=text&limit=&hgnc_dbtag=on&submit=submit";
+
+
+    private final static Logger log = LoggerFactory.getLogger(ValidateGeneSymbols.class);
+    private LoaderUtils loaderUtils;
+    private Set<String> cnvSymbols;
+    private Set<String> rnaseqSymbols;
+    private List<String> symbolsWithIssues;
+
+    @Value("${jaxpdx.cnv.url}")
+    private String cnvURL;
+
+    @Value("${jadpdx.rnaseq.url}")
+    private String rnaSeqURL;
+
+    @Autowired
+    public ValidateGeneSymbols(LoaderUtils loaderUtils) {
+        this.loaderUtils = loaderUtils;
+        this.cnvSymbols = new HashSet<>();
+        this.rnaseqSymbols = new HashSet<>();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info(args[0]);
+
+        if ("validateGeneSymbols".equals(args[0]) || "-validateGeneSymbols".equals(args[0])) {
+
+
+            long startTime = System.currentTimeMillis();
+            validateSymbols();
+            long endTime   = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
+
+            int seconds = (int) (totalTime / 1000) % 60 ;
+            int minutes = (int) ((totalTime / (1000*60)) % 60);
+
+            System.out.println("Loading finished after "+minutes+" minute(s) and "+seconds+" second(s)");
+        }
+        else{
+            log.info("Missing command");
+        }
+
+    }
+
+
+    private HashMap<String, AccessionData> loadHugoDatabase(){
+
+        String[] rowData;
+        String[] prevSymbols;
+        String[] synonyms;
+        HashMap<String, AccessionData> hmap = new HashMap<>();
+        String symbol, hgncId, entrezId, ensemblId = "";
+
+        int rows = 0;
+        int symbolConflicts = 0;
+        String symbolConf = "";
+
+        try{
+
+            URL url = new URL(DATA_FILE_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            //conn.addRequestProperty("Enctype","application/x-www-form-urlencoded");
+
+            if(conn.getResponseCode() != 200){
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String line = reader.readLine();
+            while((line = reader.readLine()) != null){
+                //HGNC_ID APPR_SYMBOL PREV_SYMBOLS SYNONYMS ENTREZ_ID ENSEMBL_ID
+
+                rowData = line.split("\t");
+                //rowData[1] = symbol, if it is not empty and if it is not a withdrawn symbol
+                if(rowData[1] != null && !rowData[1].isEmpty() ){
+
+                    symbol = rowData[1];
+
+                    if(rowData[0] != null && !rowData[0].isEmpty()){
+                        hgncId = rowData[0];
+                    }
+                    else{
+                        hgncId = "";
+                    }
+
+                    if(rowData.length>2 && rowData[2] != null && !rowData[2].isEmpty()){
+                        prevSymbols = rowData[2].split(", ");
+                    }
+                    else{
+                        prevSymbols= new String[0];
+                    }
+
+                    if(rowData.length>3 && rowData[3] != null && !rowData[3].isEmpty()){
+                        synonyms = rowData[3].split(", ");
+                    }
+                    else{
+                        synonyms = new String[0];
+                    }
+
+                    if(rowData.length>4 && rowData[4] != null && !rowData[4].isEmpty()){
+                        entrezId = rowData[4];
+                    }
+                    else{
+                        entrezId = "";
+                    }
+
+                    if(rowData.length>5 && rowData[5] != null && !rowData[5].isEmpty()){
+                        ensemblId = rowData[5];
+                    }
+                    else{
+                        ensemblId = "";
+                    }
+
+                    //put it in the hashmap with all of its prev symbols
+
+                    AccessionData ad = new AccessionData(symbol, hgncId, entrezId, ensemblId);
+
+                    if(synonyms.length>0){
+                        for(int i=0;i<synonyms.length;i++)
+                            ad.addSynonym(synonyms[i]);
+                    }
+
+                    if(prevSymbols.length>0){
+                        for(int i=0;i<prevSymbols.length;i++)
+                            ad.addPrevSymbol(prevSymbols[i]);
+                    }
+
+                    System.out.println(symbol+" "+hgncId+" "+entrezId+" "+ ensemblId);
+                    hmap.put(symbol,ad);
+
+                }
+                rows++;
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Symbol conflicts: "+symbolConflicts);
+        System.out.println("Last conflict:"+symbolConf);
+        System.out.println("Rows: "+rows);
+        System.out.println("Symbols incl. prev symbols: "+hmap.size());
+
+        return hmap;
+    }
+
+
+    private void getCnvDataFromURL(String modelId){
+
+        String json = parseURL(cnvURL+modelId);
+
+        JSONObject job = null;
+        try {
+            log.info("Loading CNV: "+modelId);
+            job = new JSONObject(json);
+            JSONObject pdxCNV = job.getJSONObject("pdxCNV");
+            JSONArray samples = pdxCNV.getJSONArray("samples");
+
+            if(samples.length()>0){
+                JSONObject cnvObj = samples.getJSONObject(0);
+                JSONArray cnv = cnvObj.getJSONArray("CNV");
+
+                for (int i = 0; i < cnv.length(); i++) {
+
+                    JSONObject geneObj = cnv.getJSONObject(i);
+                    String symbol = geneObj.getString("gene");
+                    this.cnvSymbols.add(symbol);
+
+                }
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getRnaSeqDataFromURL(String modelId){
+
+        String json = parseURL(rnaSeqURL+modelId);
+
+        JSONObject job = null;
+        try {
+            log.info("Loading rnaSeq: "+modelId);
+            job = new JSONObject(json);
+            JSONObject pdxExpression = job.getJSONObject("pdxExpression");
+            JSONArray samples = pdxExpression.getJSONArray("samples");
+
+            if(samples.length()>0){
+                JSONObject cnvObj = samples.getJSONObject(0);
+                JSONArray cnv = cnvObj.getJSONArray("Expression");
+
+                for (int i = 0; i < cnv.length(); i++) {
+
+                    JSONObject geneObj = cnv.getJSONObject(i);
+                    String symbol = geneObj.getString("gene");
+                    this.cnvSymbols.add(symbol);
+
+                }
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void createErrorList(){
+
+    }
+
+    private void validateSymbols(){
+
+        Collection<ModelCreation> models = loaderUtils.getAllModels();
+        HashMap<String, AccessionData> hugoDB = loadHugoDatabase();
+
+        for(ModelCreation m:models){
+
+            getCnvDataFromURL(m.getSourcePdxId());
+            getRnaSeqDataFromURL(m.getSourcePdxId());
+
+        }
+
+        createErrorList();
+        System.out.println("Done");
+
+    }
+
+
+    private String parseURL(String urlStr) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            URL url = new URL(urlStr);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            in.close();
+        } catch (Exception e) {
+            log.error("Unable to read from URL " + urlStr, e);
+        }
+        return sb.toString();
+    }
+
+}
