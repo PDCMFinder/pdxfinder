dca1ff90356382794513954bfe51a1d37b304883
csaba <csaba@ebi.ac.uk>
2017-11-16 12:05:56 +0000

    Disable filtering on markers

--- data-model/src/main/java/org/pdxfinder/repositories/ModelCreationRepository.java
+++ data-model/src/main/java/org/pdxfinder/repositories/ModelCreationRepository.java
@@ -27,6 +27,7 @@ public interface ModelCreationRepository extends Neo4jRepository<ModelCreation, 
             "RETURN mod")
     ModelCreation findBySampleId(@Param("sampleId") String sampleId);
 
+    /*
     //This query is being used when a user does not select an existing ontology term from autosuggest, ie: Football cancer
     @Query("MATCH (humSample:Sample)-[i:IMPLANTED_IN]-(mod:ModelCreation)" +
             "WHERE (toLower(humSample.diagnosis) CONTAINS toLower({diag}) OR {diag}='') " +
@@ -44,16 +45,31 @@ public interface ModelCreationRepository extends Neo4jRepository<ModelCreation, 
     Collection<ModelCreation> findByMultipleFilters(@Param("diag") String diag, @Param("markers") String[] markers,
                                                     @Param("dataSource") String[] dataSource, @Param("tumorType") String[] tumorType);
 
+    */
+    //disable filtering on markers
+    @Query("MATCH (humSample:Sample)-[i:IMPLANTED_IN]-(mod:ModelCreation)" +
+            "WHERE (toLower(humSample.diagnosis) CONTAINS toLower({diag}) OR {diag}='') " +
+            "AND (humSample.dataSource IN {dataSource} OR {dataSource}=[] )"+
+            "WITH mod, humSample, i " +
 
+            "MATCH (humSample)-[o:ORIGIN_TISSUE]-(t:Tissue) " +
+            "MATCH (humSample)-[ot:OF_TYPE]-(tt:TumorType) " +
+            "WHERE (tt.name IN {tumorType} OR {tumorType}=[])  " +
+            "RETURN humSample, i, mod, o, t, ot, tt ")
+    Collection<ModelCreation> findByMultipleFilters(@Param("diag") String diag, @Param("markers") String[] markers,
+                                                    @Param("dataSource") String[] dataSource, @Param("tumorType") String[] tumorType);
+
+
+    /*
     //Ontology powered search: returns less data to improve performance
     @Query("MATCH (term:OntologyTerm)<-[*]-(child:OntologyTerm)-[mapp:MAPPED_TO]-(humSample:Sample)-[i:IMPLANTED_IN]-(mod:ModelCreation) " +
             "        WHERE term.label = {query} " +
-            "        AND (humSample.dataSource IN {dataSource} OR []=[]) " +
+            "        AND (humSample.dataSource IN {dataSource} OR {dataSource}=[]) " +
             "        WITH humSample,i,mod " +
 
             "        MATCH (humSample)-[o:ORIGIN_TISSUE]-(t:Tissue) " +
             "        MATCH (humSample)-[ot:OF_TYPE]-(tt:TumorType) " +
-            "        WHERE (tt.name IN {tumorType} OR []=[])  " +
+            "        WHERE (tt.name IN {tumorType} OR {tumorType}=[])  " +
             "        WITH humSample, i, mod, o, t, ot, tt " +
 
             "        MATCH (mod)—[msr:MODEL_SAMPLE_RELATION]-(s:Sample)-[cb:CHARACTERIZED_BY]-(mc:MolecularCharacterization)-[aw:ASSOCIATED_WITH]-(ma:MarkerAssociation)-[mar:MARKER]-(m:Marker) " +
@@ -62,6 +78,21 @@ public interface ModelCreationRepository extends Neo4jRepository<ModelCreation, 
     Collection<ModelCreation> findByOntology(@Param("query") String query, @Param("markers") String[] markers,
                                               @Param("dataSource") String[] dataSource, @Param("tumorType") String[] tumorType);
 
+*/
+
+
+    //Ontology powered search: returns less data to improve performance
+    @Query("MATCH (term:OntologyTerm)<-[*]-(child:OntologyTerm)-[mapp:MAPPED_TO]-(humSample:Sample)-[i:IMPLANTED_IN]-(mod:ModelCreation) " +
+            "        WHERE term.label = {query} " +
+            "        AND (humSample.dataSource IN {dataSource} OR {dataSource}=[]) " +
+            "        WITH humSample,i,mod " +
+
+            "        MATCH (humSample)-[o:ORIGIN_TISSUE]-(t:Tissue) " +
+            "        MATCH (humSample)-[ot:OF_TYPE]-(tt:TumorType) " +
+            "        WHERE (tt.name IN {tumorType} OR {tumorType}=[])  " +
+            "        return humSample, i, mod, o, t, ot, tt ")
+    Collection<ModelCreation> findByOntology(@Param("query") String query, @Param("markers") String[] markers,
+                                             @Param("dataSource") String[] dataSource, @Param("tumorType") String[] tumorType);
 
     @Query("MATCH (n:ModelCreation) RETURN n")
     Collection<ModelCreation> getAllModels();
