8e9b96abf7c99ecd30afb55ed56b8697fcd5dbf0
csaba <csaba@ebi.ac.uk>
2018-01-31 08:18:36 +0000

    Fix json attributes for primary site

--- indexer/src/main/java/org/pdxfinder/commands/LoadWUSTL.java
+++ indexer/src/main/java/org/pdxfinder/commands/LoadWUSTL.java
@@ -145,7 +145,7 @@ public class LoadWUSTL implements CommandLineRunner {
          String tumorType = Standardizer.getTumorType(j.getString("Tumor Type"));
         
         Sample sample = loaderUtils.getSample(id, tumorType, diagnosis,
-                j.getString("Tumor Site"), j.getString("Sample Site"),
+                j.getString("Primary Site"), NOT_SPECIFIED,
                 j.getString("Sample Type"), classification, NORMAL_TISSUE_FALSE, mdaDS.getAbbreviation());
 
         pSnap.addSample(sample);
