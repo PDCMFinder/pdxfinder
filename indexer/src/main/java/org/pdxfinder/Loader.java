e562412f426efecd91d2dab5caa7fbaca2ed729b
csaba <csaba@ebi.ac.uk>
2018-03-13 10:48:47 +0000

    Exclude SearchDS component

--- indexer/src/main/java/org/pdxfinder/Loader.java
+++ indexer/src/main/java/org/pdxfinder/Loader.java
@@ -4,16 +4,22 @@ package org.pdxfinder;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.pdxfinder.services.ds.SearchDS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableNeo4jRepositories
 @EnableTransactionManagement
+@ComponentScan(value = "org.pdxfinder", excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SearchDS.class)
+}) // SearchDS has to be excluded here as it may refer to nodes that are being created during the load process
 public class Loader {
 
     private final static Logger log = LoggerFactory.getLogger(Loader.class);
