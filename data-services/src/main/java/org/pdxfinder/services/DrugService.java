e52423f7ba5dc98d330668b093c5c9890225de12
csaba <csaba@ebi.ac.uk>
2018-03-29 10:44:17 +0100

    Add dosing studies to data available service

---
+++ data-services/src/main/java/org/pdxfinder/services/DrugService.java
@@ -0,0 +1,23 @@
+package org.pdxfinder.services;
+
+import org.pdxfinder.repositories.TreatmentSummaryRepository;
+import org.springframework.stereotype.Service;
+
+/*
+ * Created by csaba on 29/03/2018.
+ */
+@Service
+public class DrugService {
+
+    private TreatmentSummaryRepository treatmentSummaryRepository;
+
+    public DrugService(TreatmentSummaryRepository treatmentSummaryRepository) {
+        this.treatmentSummaryRepository = treatmentSummaryRepository;
+    }
+
+
+    public int getDosingStudiesNumberByDataSource(String dataSource){
+
+        return treatmentSummaryRepository.findStudyNumberByDataSource(dataSource);
+    }
+}
