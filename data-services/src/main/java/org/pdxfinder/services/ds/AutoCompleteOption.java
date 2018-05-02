d22dbbfca72be6b38e7b4338bd627b6f56081800
csaba <csaba@ebi.ac.uk>
2018-05-02 13:37:26 +0100

    Rename class

--- data-services/src/main/java/org/pdxfinder/services/ds/AutoCompleteOption.java
+++ data-services/src/main/java/org/pdxfinder/services/ds/AutoCompleteOption.java
@@ -3,13 +3,13 @@ package org.pdxfinder.services.ds;
 /*
  * Created by csaba on 05/02/2018.
  */
-public class AutoSuggestOption implements Comparable<AutoSuggestOption> {
+public class AutoCompleteOption implements Comparable<AutoCompleteOption> {
 
 
     private String value;
     private String context;
 
-    public AutoSuggestOption(String value, String context) {
+    public AutoCompleteOption(String value, String context) {
         this.value = value;
         this.context = context;
     }
@@ -31,7 +31,7 @@ public class AutoSuggestOption implements Comparable<AutoSuggestOption> {
     }
 
     @Override
-    public int compareTo(AutoSuggestOption o) {
+    public int compareTo(AutoCompleteOption o) {
         return this.value.compareTo(o.value);
     }
 }
