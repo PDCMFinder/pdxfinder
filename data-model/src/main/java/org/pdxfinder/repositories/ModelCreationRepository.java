4fe5412d378aba0c6bb8907e2ac95abb3173f9f3
sprintell <abayomi@ebi.ac.uk>
2018-05-22 14:43:16 +0100

    Front end / DetailsService fix Rto resolve externalUrl for PDXI-291

--- data-model/src/main/java/org/pdxfinder/repositories/ModelCreationRepository.java
+++ data-model/src/main/java/org/pdxfinder/repositories/ModelCreationRepository.java
@@ -25,10 +25,11 @@ public interface ModelCreationRepository extends Neo4jRepository<ModelCreation, 
             "WITH model " +
             "OPTIONAL MATCH (model)-[spr:SPECIMENS]-(sp:Specimen)-[hsr:HOST_STRAIN]-(hs:HostStrain) " +
             "OPTIONAL MATCH (model)-[qar:QUALITY_ASSURED_BY]-(qa:QualityAssurance) "+
-            "WITH model, spr, sp, hsr, hs, qar, qa " +
+            "OPTIONAL MATCH (model)-[url:EXTERNAL_URL]-(ext_url:ExternalUrl) "+
+            "WITH model, spr, sp, hsr, hs, qar, qa, url, ext_url " +
             "OPTIONAL MATCH (sp)-[itr:ENGRAFTMENT_TYPE]-(it:EngraftmentType) "+
             "OPTIONAL MATCH (sp)-[isr:ENGRAFTMENT_SITE]-(is:EngraftmentSite) "+
-            "RETURN model, spr, sp, hsr, hs, itr, isr, it, is, qar, qa")
+            "RETURN model, spr, sp, hsr, hs, itr, isr, it, is, qar, qa, url, ext_url")
     ModelCreation findByDataSourceAndSourcePdxId(@Param("dataSource") String dataSource, @Param("modelId") String modelId);
 
     @Query("MATCH (model:ModelCreation) WHERE model.sourcePdxId = {modelId} AND model.dataSource = {dataSource} RETURN model ")
