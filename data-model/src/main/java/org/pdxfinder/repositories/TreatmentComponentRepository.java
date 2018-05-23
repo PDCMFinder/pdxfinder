5dc3d2e25ca53453502cd396342d0ffa43c2fd7f
csaba <csaba@ebi.ac.uk>
2018-05-23 07:39:58 +0100

    Change class to interface

--- data-model/src/main/java/org/pdxfinder/repositories/TreatmentComponentRepository.java
+++ data-model/src/main/java/org/pdxfinder/repositories/TreatmentComponentRepository.java
@@ -1,10 +1,14 @@
 package org.pdxfinder.repositories;
 
+import org.pdxfinder.dao.TreatmentComponent;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 /*
  * Created by csaba on 23/05/2018.
  */
 @Repository
-public class TreatmentComponentRepository {
+public interface TreatmentComponentRepository extends Neo4jRepository<TreatmentComponent, Long>{
+
+
 }
