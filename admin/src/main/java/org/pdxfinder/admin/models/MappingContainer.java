671d7dc7ddb7cb5b53a97084b3f3a774a1033257
csaba <csaba@ebi.ac.uk>
2018-06-18 13:56:28 +0100

    Add mapping entity and container

---
+++ admin/src/main/java/org/pdxfinder/admin/models/MappingContainer.java
@@ -0,0 +1,46 @@
+package org.pdxfinder.admin.models;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/*
+ * Created by csaba on 18/06/2018.
+ */
+public class MappingContainer {
+
+    /**
+     * A container holding mapped and unmapped entities
+     */
+    private TreeMap<Long, MappingEntity> mappings;
+
+
+
+    public MappingContainer(TreeMap<Long, MappingEntity> mappings) {
+        this.mappings = mappings;
+    }
+
+    public TreeMap<Long, MappingEntity> getMappings() {
+        return mappings;
+    }
+
+    public void setMappings(TreeMap<Long, MappingEntity> mappings) {
+        this.mappings = mappings;
+    }
+
+
+
+    public MappingEntity getEntityById(Long id){
+
+        if(mappings.containsKey(id)) return mappings.get(id);
+
+        return null;
+    }
+
+    public Long getNextAvailableId(){
+
+        int currentSize = mappings.size();
+        currentSize++;
+
+        return (long) currentSize;
+    }
+}
