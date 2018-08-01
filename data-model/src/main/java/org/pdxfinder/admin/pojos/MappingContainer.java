7c9886ce1251bd142132569c06d03c3863c0d5e1
csaba <csaba@ebi.ac.uk>
2018-08-01 10:35:15 +0100

    Add getEntityList

--- data-model/src/main/java/org/pdxfinder/admin/pojos/MappingContainer.java
+++ data-model/src/main/java/org/pdxfinder/admin/pojos/MappingContainer.java
@@ -1,6 +1,10 @@
 package org.pdxfinder.admin.pojos;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 /*
  * Created by csaba on 18/06/2018.
@@ -64,5 +68,18 @@ public class MappingContainer {
 
     public void setSize(int size) {
         this.size = size;
+    }
+
+
+    //method to transform the container something more processable by the frontend
+    public Map<String, List<MappingEntity>> getEntityList(){
+
+        List<MappingEntity> list = mappings.values().stream().collect(Collectors.toList());
+
+        Map<String, List<MappingEntity>> entityMap = new HashMap<>();
+        entityMap.put("mappings", list);
+
+
+        return entityMap;
     }
 }
