5ef8f519c51f6753d899a50913210939d60edc5e
sprintell <abayomi@ebi.ac.uk>
2018-08-06 23:08:28 +0100

    Incorporate Angular into the Admin Interace

--- admin/src/main/java/org/pdxfinder/admin/controllers/IndexController.java
+++ admin/src/main/java/org/pdxfinder/admin/controllers/IndexController.java
@@ -3,20 +3,20 @@ package org.pdxfinder.admin.controllers;
 import org.neo4j.ogm.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by abayomi on 27/06/2017.
  */
 
-@Controller
+@RestController
 public class IndexController {
 
     private final static Logger log = LoggerFactory.getLogger(IndexController.class);
 
 
-    @RequestMapping("/")
+    @RequestMapping("/api")
     String index() throws JSONException {
         log.info("In the index method");
 
