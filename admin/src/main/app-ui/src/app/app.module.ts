c0ebb0795bce5a15a201d99945086c3e0d427bb1
sprintell <abayomi@ebi.ac.uk>
2018-08-07 11:15:56 +0100

    Implement UI Navigation components

--- admin/src/main/app-ui/src/app/app.module.ts
+++ admin/src/main/app-ui/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavBarComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule
--- admin/src/main/java/org/pdxfinder/admin/controllers/IndexController.java
+++ admin/src/main/java/org/pdxfinder/admin/controllers/IndexController.java
@@ -22,6 +22,4 @@ public class IndexController {
 
         return "index";
     }
-
-
 }
