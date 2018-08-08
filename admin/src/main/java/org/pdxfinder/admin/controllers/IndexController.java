33c18e82ab59a35faad9a26cfe8fdbc3a58bcba8
sprintell <abayomi@ebi.ac.uk>
2018-08-08 16:23:15 +0100

    Forward all page requests to angular index page and preserve the route

--- admin/src/main/java/org/pdxfinder/admin/controllers/IndexController.java
+++ admin/src/main/java/org/pdxfinder/admin/controllers/IndexController.java
@@ -1,25 +1,25 @@
 package org.pdxfinder.admin.controllers;
 
-import org.neo4j.ogm.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by abayomi on 27/06/2017.
  */
 
-@RestController
+@Controller
 public class IndexController {
 
     private final static Logger log = LoggerFactory.getLogger(IndexController.class);
 
+    // Forward to home page so that the angular route is preserved.
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirect() {
 
-    @RequestMapping("/api")
-    String index() throws JSONException {
-        log.info("In the index method");
-
-        return "index";
+        return "forward:/";
     }
+
+
 }
