aae7e97f7c1c1c7e4329e5d7fb6628d69ae8bac2
sprintell <abayomi@ebi.ac.uk>
2018-08-15 17:16:41 +0100

    Integrate Centre Summary Page with back end service

--- admin/src/main/app-ui/src/app/datasource-specific/datasource-specific.component.ts
+++ admin/src/main/app-ui/src/app/datasource-specific/datasource-specific.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute  } from "@angular/router";
+import {MappingService} from "../mapping.service";
 
 @Component({
   selector: 'app-datasource-specific',
@@ -9,12 +10,34 @@ import { ActivatedRoute  } from "@angular/router";
 export class DatasourceSpecificComponent implements OnInit {
 
   public dataSource;
-  constructor(private route: ActivatedRoute) { }
+  public mappings = [];
+
+  constructor(private route: ActivatedRoute, private _mappingService: MappingService) { }
 
   ngOnInit() {
 
       // From the current url snapshot, get the source parameter and assign to the dataSource property
       this.dataSource = this.route.snapshot.paramMap.get('source');
+
+      this._mappingService.connectMissingMappingStream()
+          .subscribe(
+              data => {
+
+                  let myData = data["mappings"]; // This recieves the mappings node of the json in required format
+
+                  var count:number = 0;
+                  for (var i of myData) {
+
+                      if (myData[count].mappingValues.DataSource.toUpperCase() === this.dataSource.toUpperCase() ){
+                          this.mappings.push(myData[count]);
+                      }
+                      count++;
+                  }
+
+
+
+              }
+          );
 
   }
 
