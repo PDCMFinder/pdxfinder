2f15a7a1dbbf696cf02243f6780a102ea383572a
csaba <csaba@ebi.ac.uk>
2018-09-18 11:24:12 +0100

    Update drug standardizer rules

--- data-services/src/main/java/org/pdxfinder/services/ds/Standardizer.java
+++ data-services/src/main/java/org/pdxfinder/services/ds/Standardizer.java
@@ -210,7 +210,7 @@ public class Standardizer {
         else if(drug.equals("Topotecan")){
             return drug;
         }
-        else if(drug.equals("5-FU")){
+        else if(drug.equals("5-FU") || drug.equals("5-Fluorouracil")){
 
             return "Fluorouracil";
         }
