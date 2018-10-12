0681cc138f75786604a61a354c188909e6380eb6
csaba <csaba@ebi.ac.uk>
2018-10-12 11:35:57 +0100

    Change IRCC abbrev and name

--- indexer/src/main/java/org/pdxfinder/commands/LoadIRCC.java
+++ indexer/src/main/java/org/pdxfinder/commands/LoadIRCC.java
@@ -38,8 +38,8 @@ public class LoadIRCC implements CommandLineRunner {
 
     private final static Logger log = LoggerFactory.getLogger(LoadIRCC.class);
 
-    private final static String DATASOURCE_ABBREVIATION = "IRCC";
-    private final static String DATASOURCE_NAME = "Candiolo Cancer Institute";
+    private final static String DATASOURCE_ABBREVIATION = "IRCC-CRC";
+    private final static String DATASOURCE_NAME = "Candiolo Cancer Institute - Colorectal Cancer Collection";
     private final static String DATASOURCE_DESCRIPTION = "IRCC";
     private final static String DATASOURCE_CONTACT = "andrea.bertotti@ircc.it";
 
