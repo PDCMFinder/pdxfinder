4fe8c4327315cdaefd8e53a0fe110e8a9b2d1a41
sprintell <abayomi@ebi.ac.uk>
2018-11-01 20:10:36 +0000

    Create Objects for PDF CanvasLine and Styling Component

---
+++ web/src/main/java/org/pdxfinder/web/controllers/pdf/Canvas.java
@@ -0,0 +1,96 @@
+package org.pdxfinder.web.controllers.pdf;
+
+/*
+ * Created by abayomi on 31/10/2018.
+ */
+public class Canvas {
+
+
+    private String type;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    private String lineWidth;
+    private String lineColor;
+    private String lineCap;
+
+
+    public Canvas() {
+    }
+
+    public Canvas(String type, int x1, int y1, int x2, int y2, String lineWidth, String lineColor, String lineCap) {
+        this.type = type;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.lineWidth = lineWidth;
+        this.lineColor = lineColor;
+        this.lineCap = lineCap;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    public String getLineWidth() {
+        return lineWidth;
+    }
+
+    public void setLineWidth(String lineWidth) {
+        this.lineWidth = lineWidth;
+    }
+
+    public String getLineColor() {
+        return lineColor;
+    }
+
+    public void setLineColor(String lineColor) {
+        this.lineColor = lineColor;
+    }
+
+    public String getLineCap() {
+        return lineCap;
+    }
+
+    public void setLineCap(String lineCap) {
+        this.lineCap = lineCap;
+    }
+}
