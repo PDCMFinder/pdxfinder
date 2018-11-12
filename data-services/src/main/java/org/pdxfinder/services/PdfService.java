13919e2d5c157351736a9ce7f9917e835673f776
sprintell <abayomi@sprintell.org>
2018-11-12 15:17:01 +0000

    PDF: Add the source in the header, Reduce size of pdx logo

--- data-services/src/main/java/org/pdxfinder/services/PdfService.java
+++ data-services/src/main/java/org/pdxfinder/services/PdfService.java
@@ -37,7 +37,7 @@ public class PdfService {
         List<Object> row1Column1Contents = new ArrayList<>();
 
         // Row 1 Column 1 1st content
-        Object firstContent = logoHeaderDesign(data.getModelId(), modelUrl, data.getMappedOntology());
+        Object firstContent = logoHeaderDesign(data, modelUrl);
         row1Column1Contents.add(firstContent);
 
         row1Column1Contents.add(
@@ -483,7 +483,7 @@ public class PdfService {
     }
 
 
-    public Map<String, Object> logoHeaderDesign(String modelID, String qrCodeData, String diagnosis) {
+    public Map<String, Object> logoHeaderDesign(DetailsDTO data, String qrCodeData) {
 
 
         //String qrCodePath = pdf.generateQRCode(modelID, qrCodeData);
@@ -491,7 +491,7 @@ public class PdfService {
         String base64Image = "iVBORw0KGgoAAAANSUhEUgAAARsAAACgCAYAAAA8V8gjAAAACXBIWXMAABcSAAAXEgFnn9JSAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAADgZJREFUeNrsnb9uG8kZwJeGkCqBmLyAeAhwrRi4DUCmShPAPuQBxCviFCksP4FWfYDIRRqnMPkERwPXpPIKSBXgcFR7wMHkC+QoJNU1zHzSUEfTu+K/md35Zn4/YEGbslc7szu//b7ZmdnWYrHIADTRejoamI/nZuub7ZgaaZxbs41lW3xzNq48b8gGFElGBHNlthNqI1hmZhsY6RTrP3hC3YAS0YhkvkI0wSPn5705XzmRDWgUzdB8nFET6hiZCGdAZANaRJMjGrWc2f41IhsIXjQd8/GBmlCNdB53TYQzJbKBkMmpAvUcL88jkQ2EGtW0zccP1EQc0Y2JbNpH1AMEynOqIJ7oRoYtkEZBqHSogqjoIhsIlT5VEBVtZAMARDYAEA9HrVarCPj4Vo9tYrb5YrEI8nhNPXaz+3k71CNAmWzM1gv4+HoljVo+bmwDGgfUaNoB1+WmeixMPY5pDuATrWnUqdleZjLhq9Wami03W5vTuXc9fmXqb262odk6qVVC+xc/y/I/d5Mq7/Dyt3efyGY3ZJbphdnupIM/9kZGesocpA8pSUcaXPHm99nFi9O7BphKec/+8Ou7zzqF8ySyxnJhGsnE9p/A/iylk6fQ8E4//9V9oU0DjFk46+WVzzqFE+PTKEkNCtNQBjjjYJby7sTe8B4sG6lwqspbp3BiffQtUc5bhONM3lFFi1UNL1bhbCpvXcKJfZzNW1IqZ/KOIlrc1PBiE8625a1DOCkM6it4UuU0WuzG3vBiEc6u5fUtnCeJNJIhrnAq727sDU+7cPYtr0/hpDJd4ZlpIH084U7emqLFfRueVuEcWl5fwklpblSOJ5xxqqU+D2142oTjqrw+hJOSbHp0FjvlZejRoquGp0U4rsvrWjipzfo+xxFOuUql4YUuHF/ldSmc1GTDUpOO06kUxzKFJhxfolky+e6HbP7fHw/ej4s1iG88RAwSnnesHFy+y/lYUqnFYjEJ9DoeZW6fnHVWNqlTH2+TzLMAn/ZJ4+i/+Ke3RijCEQYX/4paNKOvv3dWRhey8bE2yt3+7BMPEdmF4+gmVNlMfS6ZYacdiBxcvvTtRKIbc9wIB9HoTaPMBSwik8bxpeOoKUlMXYrMJO35zEakrgg2lVoK5+a7/0SVUmkTTfCyWWkkcte8dLS75J9IWelIPbxztMteyJM1YxOORtGokY1tIBLhzBzs6pjpCx9FJK4inKCf9MUiHK2iUSUbi6t+Acbb2DTVYQoU/JM+7cLRLJqUZUNk85NwpLN85GBXJxoGTWoVjnbRqJON9DU4SqWIbD4md7SfvobCahNODKLRGNkIE9zgReIuohs1gya1CCcW0SAbWMXFq1x6mgocunBiEo1W2YCf6EZkc3vofrQt5RGqcGITjVbZzFPpW2iAwsE+1PWHhSacGEVDGgXIJjDhxCoarbLpBBIdxcgkkPOTpHBiFk3KsiE6KsHRJNCe5jpoSjixi0arbMAvB09f0D4dpG7hpCAarbJh9K/ntuZgH+oHTdYlnFREo1U2jP71i4tUqhNDRdQhnOnXf0xCNCnLhj4bv3RiKYhv4Rz/3M8L4UITjTrZ2DVTXCwTytMov5FNXHmlZ+GkIBqNkY2ruTdENqS6UQonVNFolI2TBZrsOi7gjyg78UMXTsiiUSUbk0KJaFy8HeAaFzwqYtIohcIJXTRqZGMn9/2NFAoQjk7RBC8bGRxmttz88b3D3XLn9k8v9gKGIhwtohFcvDeqY4XgOufverpokQ04FY7PQXmxiMaVbKQf5UJJed/ROQwxCEebaIJPozwwpnmA9pRKo2iQDYAy4WgVTWqyGZFCASCbOsg53eAL37O3lzT1bnFks1tUM6VJgGbRaBdOCrK5JaqBWESjWTgpyOacqAZiEo1W4cQuG0mfhjSL2rmJvYBNi0ajcGKWjYhmQLvfDUfrB0f91C8U0WgTTqyyeYVo9oZlVxWJRpNwjiK7FmT5iAF9NJCSaFaFI4Q66M+FbCQ/l7Vm+tlPEyjl87TOlMlsQ9ZicULHwT6iOw+hi0aDcFzIZm4beVHRB9C3f+yvhOntA4U0y+7XpZHpB2NGBgcnG0SDcOpPo1aijcq7nRHSUkDZSnS0jshFpDJBLl5x0WcTTRrrWzS3//vRyxsWQhROEH02Rh7rq+cxYVJ3ZBOFbOp4gdz5X//t7XeEJhxevwvruOhrU7/0al1vqmzq3eLIBhplpX/toMxAe5pb9ytxUxEOsoFVkn/baFPv3k5BOMgGVnER2aiVTVOiSUU4yAaWKZQ8BXyWqmyaFk0KwkE2sMTVq40LRLOfaGIXDrKBJQMH+5hpmyoSmmhiFg6ygeWgShfv6FIV1YQqmliFg2xAyFNLoUIXTYzCQTZENf3MTcewoGLktxbRxCYcZJO2aOQJ1NDR7lS8bVSbaGISDrJJG4lETlKJarSKJhbhIJtEIxqzSUTTc7TL29DXetYumhiEg2zSE408eSrkunK42ytEU9/Maq3CQTbpSKZvo5lvM/erKAYrm9hEo1k4RzTDoBAh5A7317GbRDPHno75dagdw7GKZl04WtbDQTZh0cvc9aPUQbBvG41dNBqFQxoFB6VPKS7RGopo6kqpup//8k7eyAaa4saIJg/14Hw1wNBE47u8sj/Zr+wf2UBT6dMg9IN03QBDFY2v8roUDbKBfTkvWaQ+6gYYumhcl9e1aJAN7NXuQh/A57oBahGNq/L6EA2ygX1EM9B44Ps2QG2iObS8vkSDbCAJ0ezbALWKZt/y+hQNsoFkRLNrA9Quml3L61s0yAY2IU+dvoxFNNs2wFhEs2156xANsoHHuDZbV1tn8KENMDbRbCpvXaJBNlDGzGxfGMn0tS1efmgDjFU0VeWtUzTIBlZ5ZyXTMds4lUIvG+Dlm5uoRbNeXhFrnaIRWmZbHBpuy10w9ZZq1/J9ryyCKew2Dm2OU+vpSI6rl0E0abnM+r48cCdT6vGhHi4DP0YZ9StSmaQ4gRKa5SjkyXSasP0b1CVABfTZAACyAYBomCAbCJWCKoiKObKBUJlSBUQ2AHUwpgqi4XbxzdkY2UCQmItTHs2PqIl4bhzIBkImpwr0RzVmO0c2EHp0MzUfr6gJ1ZzbKDVrLRYLqgOCpvV0NMzcvi4Y6mFkRDNY/oXIBjREOHLBvqYmVHG5KhoiG9AW4fTNh0Q5J9RGsMg6SLkRTfHJ+UM2oFA6z82HbH3EEwTLFQSGZZJBNgBQ700C2QAAsgEAZAMAgGwAANkAALIBAEA2AIBsAACQDQAgGwBANgAAyAYAkA0AALIBAGQTFy/+9JdBdr/I8+kj/0wWgp6UfC/fyZqtxZt//L3Y8/fn5uNi7etrs7++g7KV7Vv4zOx/uuO+yi6+3+1b7orfIfvq7fnfb7L7dVqudi3bHscp50aum03Ln16XfDddbuY4h57O77bI8Q3LjuMINTi/aIbZduvlHlc0gtXvCkVFl3L3Izudp3Z7ac7rpWlAuceb09st//lj18y1PQ9NIsfSM2WSm+3A1NnDDZU1iN1eNHmW7sLcywssVi5slOQjonkbYX2JpMemfG1k44fzxMufr15ckQrVdeQwiLi+ZMnWnDTK/R2qa1OjbfPsTUwVVsOxDeOfB36cow31K9FG1fk8E+E47FeqSj2lv2i+474mnurrekNK37blKOujHEjEb+prjmzcUXVH/81q3poAzyQ1cNnJ64GtZPFIZ2meueufKluw/ZU5vquA6qvYpr+qou/p2NYV7/r2zG1ionlozDGkU7aBXVakU12Pv3qitL6GNiJb566ukI1fJomWW+7WUfRfWeHMytIDj7+zUFxlBbKBurnwfPevk7KUps8pLmVe1cWAbMBrOhVJOcru1qec3t1ANuCC66z8idtpDGNvqvrdIorckA2oYpDdT8FYR8bedCIo301VegAb62SObMDl3X+alfdtHFd8r42yvogOZ/4TysZY3UWGjLPxS69isuHyTll2AQ8PnUzXoHAkipGLbb0/Q8bePDc/H0d2fr3I5pFrRp6KTSvqvt9kRdihDnJTOUE24VHVwVhEkE59WyZRSadkJKnSckkj7zV8DCcVjdk38mTxkJngBWkU+Ihu5C72uiKdyhUXbcrZ3YvR8gaDbMAHeVY+EO6lneWsEZ487Y48MHh4GkkaVR+zDXfHIpI0SqKbuZ0n877kx1dKG267ocY62RBthRpxvcvu17OZI5t6uG66465B4RRGOHLBPVv70amdBZxHUMzCU921QkuFso8XR2vbG0YnK+9DKp18jGzAJwN7511fquHCw7owvuklfB6ndr7WR3K146c+VKScn8iGPhvwmk5l1RMy1cimaqSw8gmTLs7v1KZL65RGrcgGfF+QIpVr5ZFCWSp8w9m9o2zA5onts0M20Eg6dav4+Muis4LT+hDdXW8T3SAbqCvcVjllwU4kLesEHXNmH02bPolukA3UJZxcW+ph+2rKGtLMZ3+Nttnkti5mmySEbKDudEqFZOz6wzLt4njLO7nTSEEGPypbWrUquukv/8Kjb6jzDjgxF59MZXjZ8KG8N8ex7/+9cTxRdlaSpj2zW7bjcTY2rkvqxAr6pERCfSIbaOoOOFN67NLJ7fo1NUVk53ad3jK6QTZQ9x1wnul8MZsIsu/hnd9XEZ3ecVaxgBqygaaEI3fzd4qiGUn9uj5ey2P3+UWme2jA6o3kqiq6oc/GHVLR6+MNmnqVy9Tjsbja9yArf3zser2bfctd2LKOfa/BI4uK2aH/UieScrQr6n264buJp/O7SzR3lZUPguz/X4ABAPz28m2wmuAcAAAAAElFTkSuQmCC";
         //pdf.base64EncodeToString(pdf.imageToByteRemote(logoURL));
 
-        String qrCodeImage = pdf.base64EncodeToString(pdf.generateQRCode(modelID, qrCodeData));
+        String qrCodeImage = pdf.base64EncodeToString(pdf.generateQRCode(data.getModelId(), qrCodeData));
 
         // delete the qrCode File:
         //pdf.deleteFile(qrCodePath);
@@ -510,8 +510,8 @@ public class PdfService {
 
         List<Column> columns = new ArrayList<>();
         Column column = new Column();
-        column.setWidth(90);
-        column.setMargin(Arrays.asList(20, 15, 0, 5));
+        column.setWidth(60);
+        column.setMargin(Arrays.asList(20, 25, 0, 5));
         column.setImage(appendBase64 + base64Image);
         columns.add(column);
 
@@ -524,13 +524,14 @@ public class PdfService {
 
 
         // create row 1 and add to table body
-        text = new Text(modelID, "h1");
+        text = new Text(data.getModelId(), "h1");
         text.setAlignment("center");
         tableRow = Arrays.asList(text);
         tableBody.add(tableRow);
 
         // create row 2 and add to table body
-        text = new Text(diagnosis, "h2");
+        text = new Text(data.getMappedOntology()+"\n"+data.getDataSource(), "h2");
+        text.setAlignment("center");
         tableRow = Arrays.asList(text);
         tableBody.add(tableRow);
 
