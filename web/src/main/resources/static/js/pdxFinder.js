aaf6457c9d36de86238bceeea42b1311025e6f96
sprintell <abayomi@sprintell.org>
2018-12-19 14:09:40 +0000

    Optimize Javascript

--- web/src/main/resources/static/js/pdxFinder.js
+++ web/src/main/resources/static/js/pdxFinder.js
@@ -7,7 +7,7 @@
  * Checks the filters and collects the parameters that are selected, t
  * hen constructs the url and redirects the user to that url
  */
-function redirectPage(webFacetSections){
+function redirectPage(webFacetSections) {
 
     var no_parameters = true;
     var url = "?"
@@ -15,129 +15,96 @@ function redirectPage(webFacetSections){
     var searchField = jQuery("#query");
     //check if main search field has anything
     if (searchField.val() != null && searchField.val() != "") {
-        url+="query="+searchField.val();
+        url += "query=" + searchField.val();
         no_parameters = false;
     }
 
     //check elements with filter class
 
-    jQuery(".filter").each(function(){
+    jQuery(".filter").each(function () {
         var id = jQuery(this).attr("id");
 
         //characters we want to see as values
         var reg = /[^A-Za-z0-9 _-]/;
 
-        if (jQuery(this).is(':checked')){
+        if (jQuery(this).is(':checked')) {
 
             var res = id.split("__");
 
-            if(!no_parameters){
-                url = url+"&";
+            if (!no_parameters) {
+                url = url + "&";
             }
 
-            url = url+res[0]+"="+res[1];
+            url = url + res[0] + "=" + res[1];
             no_parameters = false;
 
         }
-        else if(jQuery(this).is("input:text")){
+        else if (jQuery(this).is("input:text")) {
             return;
         }
     });
 
 
 
+    var twoParamUnlinkedFilters = getComponentsFromWebFacetSection(webFacetSections, 'TwoParamUnlinkedFilter');
+    twoParamUnlinkedFilters.forEach(function (filterComponent) {
 
+            options1List = filterComponent.options1;
+            options2List = filterComponent.options2;
+            componentId1 = filterComponent.urlParam + "_" + (filterComponent.param1Name).toLowerCase();
+            componentId2 = filterComponent.urlParam + "_" + (filterComponent.param2Name).toLowerCase();
+            urlKey = filterComponent.urlParam;
 
+            for (var i = 1; i < 20; i++) {
 
+                var component1Choice = jQuery("#" + componentId1 + i);
+                var component2Choices = jQuery("#" + componentId2 + i);
 
-    webFacetSections.forEach(buildURL);
+                if (component1Choice.val() != null && component1Choice.val() != "NULL") {
 
-    function buildURL(webFacetSection, index) {
+                    for (var j = 0; j < component2Choices.val().length; j++) {
 
-        var filterComponents = webFacetSection.filterComponents;
+                        if (!no_parameters) {
+                            url = url + "&";
+                        }
 
-        // Retrieve All the FilterComponents and their contents
-        filterComponents.forEach(function(filterComponent){
+                        if (options2List.length == component2Choices.val().length) {
 
-            if (filterComponent.type === 'TwoParamUnlinkedFilter'){
+                            url += urlKey + "=" + component1Choice.val() + "___ALL";
+                            no_parameters = false;
+                            break;
+                        } else {
+                            url += urlKey + "=" + component1Choice.val() + "___" + component2Choices.val()[j];
+                            no_parameters = false;
 
-                options1List = filterComponent.options1;
-                options2List = filterComponent.options2;
-                componentId1 = filterComponent.urlParam+"_"+(filterComponent.param1Name).toLowerCase();
-                componentId2 = filterComponent.urlParam+"_"+(filterComponent.param2Name).toLowerCase();
-                urlKey = filterComponent.urlParam;
-
-                intializeTwoParamUnlinkedFilterOptionOneList(dataList, componentId1);
-                intializeTwoParamUnlinkedFilterOptionTwoList(componentId2);
-
-
-
-                for (var i=1; i<20; i++){
-
-                    var component1Choice = jQuery("#"+componentId1+i);
-                    var component2Choices = jQuery("#"+componentId2+i);
-
-                    if (component1Choice.val() != null && component1Choice.val() != "NULL")
-                    {
-
-                        for (var j=0; j<component2Choices.val().length; j++){
-
-                            if (!no_parameters) {
-                                url = url + "&";
-                            }
-
-                            if(options2List.length == component2Choices.val().length){
-
-                                url += urlKey+"=" + component1Choice.val() + "___ALL";
-                                no_parameters = false;
-                                break;
-                            }else{
-                                url += urlKey+"=" + component1Choice.val() + "___"+component2Choices.val()[j];
-                                no_parameters = false;
-
-                            }
                         }
                     }
-
                 }
 
-
             }
-        });
-    }
+    });
 
 
 
 
 
+    for (var i = 1; i < 20; i++) {
 
+        var geneFilter = jQuery("#geneFilter" + i);
+        var variantFilter = jQuery("#variantFilter" + i);
 
-
-
-
-
-
-
-
-
-    for (var i=1; i<20; i++){
-
-        var geneFilter = jQuery("#geneFilter"+i);
-        var variantFilter = jQuery("#variantFilter"+i);
-
-        if (geneFilter.val() != null && geneFilter.val() != "")
-        {
+        if (geneFilter.val() != null && geneFilter.val() != "") {
             var allVariants = getVariantSize(geneFilter.val());
-            for (var j=0; j<variantFilter.val().length; j++){
+            for (var j = 0; j < variantFilter.val().length; j++) {
                 if (!no_parameters) {
                     url = url + "&";
                 }
-                if(allVariants.length == variantFilter.val().length){
+                if (allVariants.length == variantFilter.val().length) {
                     url += "mutation=" + geneFilter.val() + "___MUT" + "___ALL";
                     no_parameters = false;
                     break;
-                }else{
-                    url += "mutation=" + geneFilter.val() + "___MUT" + "___"+variantFilter.val()[j];
+                } else {
+                    url += "mutation=" + geneFilter.val() + "___MUT" + "___" + variantFilter.val()[j];
                     no_parameters = false;
 
                 }
@@ -145,7 +112,6 @@ function redirectPage(webFacetSections){
         }
 
     }
-
 
 
     // Add all diagnosis filters to the URL
@@ -168,6 +134,8 @@ function redirectPage(webFacetSections){
     });
 
     window.location.replace(url);
+
+
 }
 
 
@@ -176,41 +144,86 @@ function redirectPage(webFacetSections){
 
 
 
-function intializeTwoParamUnlinkedFilterOptionOneList(dataList, componentId){
+
+
+
+/****************************************************************
+ *         TWO PARAM UNLINKED FILTER SECTION STARTS             *
+ ****************************************************************/
+
+
+function getComponentsFromWebFacetSection(webFacetSections, desiredFilterType) {
+
+    filterComponentsArray = [];
+    webFacetSections.forEach(getFilterComponents);
+
+    function getFilterComponents(webFacetSection, index) {
+
+        var filterComponents = webFacetSection.filterComponents;
+
+        // Retrieve All the FilterComponents and their contents
+        filterComponents.forEach(function(filterComponent) {
+
+            if (filterComponent.type === desiredFilterType) {
+                filterComponentsArray.push(filterComponent);
+            }
+        });
+    }
+
+    return filterComponentsArray;
+}
+
+
+
+
+
+function intializeFilters(webFacetSection, index) {
+
+    var filterComponents = webFacetSection.filterComponents;
+
+    // Retrieve All the FilterComponents and their contents
+    filterComponents.forEach(function(filterComponent){
+
+        if (filterComponent.type === 'TwoParamUnlinkedFilter'){
+
+            dataList = filterComponent.options1;
+            componentOneId = filterComponent.urlParam+"_"+(filterComponent.param1Name).toLowerCase();
+            componentTwoId = filterComponent.urlParam+"_"+(filterComponent.param2Name).toLowerCase();
+            filterButton = filterComponent.urlParam+"_"+(filterComponent.param1Name).toLowerCase()+'_button';
+
+            intializeTwoParamUnlinkedFilterComponents(dataList, componentOneId, componentTwoId);
+
+            //Add eventlistener to each TwoParamUnlinkedFilter filter class
+            jQuery('#'+filterButton).click(function () {
+                redirectPage(webFacetSections);
+            });
+        }
+    });
+}
+
+
+function intializeTwoParamUnlinkedFilterComponents(dataList, componentOneId, componentTwoId) {
 
     dataList = dataList.sort();
 
-    /*$('#'+componentId).autocomplete({
-        source: [dataList]
-    });*/
-
     for (var i = 1; i <= 20; i++) {
-        $('#'+componentId + i).autocomplete({
+
+        $('#' + componentOneId + i).autocomplete({
             source: [dataList]
         });
-    }
-}
 
-function intializeTwoParamUnlinkedFilterOptionTwoList(componentId){
-
-    /*$('#'+componentId).change(function () {
-        //console.log($(this).val());
-    }).multipleSelect({
-        placeholder: "Responses"
-    });*/
-
-    for (var i = 1; i <= 20; i++) {
-        $('#'+componentId+ i).change(function () {
+        $('#' + componentTwoId + i).change(function () {
             //console.log($(this).val());
         }).multipleSelect({
             placeholder: "Responses"
         });
     }
+
+
 }
 
 
-
-function selectAllOptionsInMyComponent2(myComponent2Id,options2List) {
+function selectAllOptionsInMyComponent2(myComponent2Id, options2List) {
 
     var newOptions = "";
 
@@ -226,7 +239,7 @@ function selectAllOptionsInMyComponent2(myComponent2Id,options2List) {
     }
 
     // Empty and reload the select Box with the selected options
-    var select = $('#'+myComponent2Id);
+    var select = $('#' + myComponent2Id);
     select.empty().append(newOptions);
 
     $(function () {
@@ -243,13 +256,13 @@ function displayMore(divId) {
 
     for (var i = 2; i <= 20; i++) {
 
-        var hiddenDiv = divId+'_hidden'+i;
-        var textComponentId = divId+i;
+        var hiddenDiv = divId + '_hidden' + i;
+        var textComponentId = divId + i;
 
         var hiddenDivDOM = document.getElementById(hiddenDiv);
         var textComponentDOM = document.getElementById(textComponentId);
 
-        if (hiddenDivDOM.style.display === "none"){
+        if (hiddenDivDOM.style.display === "none") {
 
             hiddenDivDOM.style.display = "block";
             textComponentDOM.value = "";
@@ -260,20 +273,9 @@ function displayMore(divId) {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/****************************************************************
+ *         TWO PARAM UNLINKED FILTER SECTION ENDS             *
+ ****************************************************************/
 
 
 
@@ -291,7 +293,7 @@ function displayMore(divId) {
 var geneticVar = 1;
 var counter = 1;
 
-function loadGeneTextFields(){
+function loadGeneTextFields() {
 
     var keysAreMarkers = Object.keys(mutatedMarkersAndVariants);
     $('#geneFilter1').autocomplete({
@@ -325,7 +327,6 @@ function loadVariants(selectedMarker, compNumber) {
 }
 
 
-
 function addMarkerAndVariants(param, startIndex) {
     if (startIndex != 2 && counter == 1) {
         geneticVar = startIndex;
@@ -345,12 +346,6 @@ function getVariantSize(selectedMarker) {
     var valuesAreVariants = mutatedMarkersAndVariants[selectedMarker];
     return valuesAreVariants;
 }
-
-
-
-
-
-
 
 
 function clearFacet() {
