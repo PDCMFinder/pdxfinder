bf6d5a47187c01efa1198ebbf773f7577dda638f
sprintell <abayomi@sprintell.org>
2018-12-24 14:25:07 +0000

    Clean Up

--- web/src/main/resources/static/js/pdxFinder.js
+++ web/src/main/resources/static/js/pdxFinder.js
@@ -112,6 +112,7 @@ function redirectPage(webFacetSections) {
 
     }
 
+    
 
     // Add all diagnosis filters to the URL
     jQuery(".diagnosis").each(function () {
@@ -174,6 +175,7 @@ function intializeFilters(webFacetSection, index) {
         }
     });
 }
+
 
 
 function initializeTwoParamFilterComponents(dataList, componentOneId, componentTwoId) {
