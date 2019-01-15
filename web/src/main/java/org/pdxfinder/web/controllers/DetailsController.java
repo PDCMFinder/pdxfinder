e872a146302c3e7bc435fb75a28438e72f175c3f
sprintell <abayomi@sprintell.org>
2019-01-15 15:35:12 +0000

    Fix molecular data show (count) when we are not displaying the dataset yet, should show 0 .

--- web/src/main/java/org/pdxfinder/web/controllers/DetailsController.java
+++ web/src/main/java/org/pdxfinder/web/controllers/DetailsController.java
@@ -22,7 +22,7 @@ import java.util.Set;
 /*
  * Created by abayomi on 09/05/2018.
  */
-@Controller
+@RestController
 public class DetailsController {
 
 
@@ -39,14 +39,14 @@ public class DetailsController {
 
 
     @RequestMapping(value = "/pdx/{dataSrc}/{modelId:.+}")
-    public String details(Model model,
+    public DetailsDTO details(Model model,
                           @PathVariable String dataSrc,
                           @PathVariable String modelId,
                           @RequestParam(value = "page", defaultValue = "0") Integer page,
                           @RequestParam(value = "size", defaultValue = "15000") Integer size) {
 
-        model.addAttribute("data", detailsService.getModelDetails(dataSrc, modelId, page, size, "", "", ""));
-        return "details";
+       // model.addAttribute("data", detailsService.getModelDetails(dataSrc, modelId, page, size, "", "", ""));
+        return detailsService.getModelDetails(dataSrc, modelId, page, size, "", "", ""); //"details";
     }
 
 
