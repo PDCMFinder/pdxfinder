828bc16c5f0efc4e61efc48bb667dfa5f50b5eea
csaba <csaba@ebi.ac.uk>
2019-02-28 17:56:47 +0000

    toString for MLE

--- data-services/src/main/java/org/pdxfinder/services/reporting/MarkerLogEntity.java
+++ data-services/src/main/java/org/pdxfinder/services/reporting/MarkerLogEntity.java
@@ -15,4 +15,36 @@ public class MarkerLogEntity extends LogEntity{
         this.suggestedMarkerSymbol = suggestedMarkerSymbol;
         this.reasonForChange = reasonForChange;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        MarkerLogEntity that = (MarkerLogEntity) o;
+
+        if (!usedMarkerSymbol.equals(that.usedMarkerSymbol)) return false;
+        if (!suggestedMarkerSymbol.equals(that.suggestedMarkerSymbol)) return false;
+        return reasonForChange.equals(that.reasonForChange);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + usedMarkerSymbol.hashCode();
+        result = 31 * result + suggestedMarkerSymbol.hashCode();
+        result = 31 * result + reasonForChange.hashCode();
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        return "MarkerLogEntity{" +
+                "usedMarkerSymbol='" + usedMarkerSymbol + '\'' +
+                ", suggestedMarkerSymbol='" + suggestedMarkerSymbol + '\'' +
+                ", reasonForChange='" + reasonForChange + '\'' +
+                '}';
+    }
 }
