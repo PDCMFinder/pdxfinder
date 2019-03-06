312293223f17f5e26394cf7f2e5cde3cfdb4b791
sprintell <abayomi@sprintell.org>
2019-03-06 14:26:06 +0000

    1.) Implemented Generalized ConfigurationProperties Bean for all data sources to Inject loader.properties values 2.) This saves us from using @Value annotation to inject the .properties value one by one

---
+++ indexer/src/main/java/org/pdxfinder/commands/dataloaders/LoaderProperties.java
@@ -0,0 +1,147 @@
+package org.pdxfinder.commands.dataloaders;
+
+/*
+ * Created by abayomi on 06/03/2019.
+ */
+public class LoaderProperties {
+
+    protected String dataSourceAbbreviation;
+    protected String dataSourceName;
+    protected String dataSourceDescription;
+    protected String dataSourceContact;
+    protected String providerType;
+    protected String accessibility;
+
+    protected String nsgBsName;
+    protected String nsgBsSymbol;
+    protected String nsgbsURL;
+    protected String nsBsName;
+    protected String nsBsSymbol;
+    protected String nsBsURL;
+
+    protected String dosingStudyURL;
+    protected String sourceURL;
+    protected String normalTissueFalse;
+
+
+    public String getDataSourceAbbreviation() {
+        return dataSourceAbbreviation;
+    }
+
+    public void setDataSourceAbbreviation(String dataSourceAbbreviation) {
+        this.dataSourceAbbreviation = dataSourceAbbreviation;
+    }
+
+    public String getDataSourceName() {
+        return dataSourceName;
+    }
+
+    public void setDataSourceName(String dataSourceName) {
+        this.dataSourceName = dataSourceName;
+    }
+
+    public String getDataSourceDescription() {
+        return dataSourceDescription;
+    }
+
+    public void setDataSourceDescription(String dataSourceDescription) {
+        this.dataSourceDescription = dataSourceDescription;
+    }
+
+    public String getDataSourceContact() {
+        return dataSourceContact;
+    }
+
+    public void setDataSourceContact(String dataSourceContact) {
+        this.dataSourceContact = dataSourceContact;
+    }
+
+    public String getProviderType() {
+        return providerType;
+    }
+
+    public void setProviderType(String providerType) {
+        this.providerType = providerType;
+    }
+
+    public String getAccessibility() {
+        return accessibility;
+    }
+
+    public void setAccessibility(String accessibility) {
+        this.accessibility = accessibility;
+    }
+
+    public String getNsgBsName() {
+        return nsgBsName;
+    }
+
+    public void setNsgBsName(String nsgBsName) {
+        this.nsgBsName = nsgBsName;
+    }
+
+    public String getNsgBsSymbol() {
+        return nsgBsSymbol;
+    }
+
+    public void setNsgBsSymbol(String nsgBsSymbol) {
+        this.nsgBsSymbol = nsgBsSymbol;
+    }
+
+    public String getNsgbsURL() {
+        return nsgbsURL;
+    }
+
+    public void setNsgbsURL(String nsgbsURL) {
+        this.nsgbsURL = nsgbsURL;
+    }
+
+    public String getNsBsName() {
+        return nsBsName;
+    }
+
+    public void setNsBsName(String nsBsName) {
+        this.nsBsName = nsBsName;
+    }
+
+    public String getNsBsSymbol() {
+        return nsBsSymbol;
+    }
+
+    public void setNsBsSymbol(String nsBsSymbol) {
+        this.nsBsSymbol = nsBsSymbol;
+    }
+
+    public String getNsBsURL() {
+        return nsBsURL;
+    }
+
+    public void setNsBsURL(String nsBsURL) {
+        this.nsBsURL = nsBsURL;
+    }
+
+    public String getDosingStudyURL() {
+        return dosingStudyURL;
+    }
+
+    public void setDosingStudyURL(String dosingStudyURL) {
+        this.dosingStudyURL = dosingStudyURL;
+    }
+
+    public String getSourceURL() {
+        return sourceURL;
+    }
+
+    public void setSourceURL(String sourceURL) {
+        this.sourceURL = sourceURL;
+    }
+
+    public String getNormalTissueFalse() {
+        return normalTissueFalse;
+    }
+
+    public void setNormalTissueFalse(String normalTissueFalse) {
+        this.normalTissueFalse = normalTissueFalse;
+    }
+
+}
