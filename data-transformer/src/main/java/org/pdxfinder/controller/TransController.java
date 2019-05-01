7863ac8f749f6c96bd79ebfd5ac9b3f61acb686e
sprintell <abayomi@sprintell.org>
2019-05-01 17:52:59 +0100

    Transform and Update missing sampleId, sampleOrigin, Passages and Platform for Curie-LC CNA Data from metadata

--- data-transformer/src/main/java/org/pdxfinder/controller/TransController.java
+++ data-transformer/src/main/java/org/pdxfinder/controller/TransController.java
@@ -122,7 +122,7 @@ public class TransController {
 
     private String omicFile= System.getProperty("user.home") + "/Downloads/NGS-LOADER/ncicancergenepaneldata_MARCH2019.csv";
 
-    @RequestMapping("/rewrite-omic-data")
+    @RequestMapping("/rewrite-pdmr-omic-data")
     public Object downloads() {
 
         List<Map<String, String>> dataList = utilityService.serializeCSVToMaps(omicFile);
@@ -157,8 +157,6 @@ public class TransController {
                 removedList.add(data);
             }
 
-
-
         }
 
         dataList.removeAll(removedList);
@@ -173,6 +171,66 @@ public class TransController {
 
 
 
+
+    @RequestMapping("/rewrite-curie-lc-cna")
+    public Object curieTransform() {
+
+        String cnaFile= System.getProperty("user.home") + "/Downloads/curie-cna.xlsx";
+
+        List<Map<String, String>> dataList = utilityService.serializeExcelDataNoIterator(cnaFile,0,1);
+
+
+        List<String> xlsHead = Arrays.asList("datasource","Model_ID","Sample_ID","sample_origin","Passage","host_strain_name","chromosome","seq_start_position","seq_end_position",
+                "Probe_ID_affymetrix","hgnc_symbol","ucsc_gene_id","ncbi_gene_id","ensembl_gene_id","log10R_cna","log2R_cna","copy_number_status",
+                "gistic_value_cna","genome_assembly","Platform");
+
+
+        for (Map<String, String> data : dataList) {
+
+            String modelID = data.get("Model_ID");
+
+            if (modelID.equals("LCF4")){
+
+                data.put("Sample_ID","LCF4p09/09/2009");
+                data.put("sample_origin","engrafted tumor");
+                data.put("Passage","4");
+                data.put("Platform","CGH array");
+
+            }
+
+            if (modelID.equals("LCF9")){
+
+                data.put("Sample_ID","LCF9p16:19/05/2015");
+                data.put("sample_origin","engrafted tumor");
+                data.put("Passage","16");
+                data.put("Platform","CGH array");
+
+            }
+
+            if (modelID.equals("LCF16")){
+
+                data.put("Sample_ID","LCF16p27:26/08/2016");
+                data.put("sample_origin","engrafted tumor");
+                data.put("Passage","27");
+                data.put("Platform","CGH array");
+
+            }
+
+            if (modelID.equals("LCF26")){
+
+                data.put("Sample_ID","LCF26p3:23/09/2011");
+                data.put("sample_origin","engrafted tumor");
+                data.put("Passage","3");
+                data.put("Platform","CGH array");
+
+            }
+
+        }
+
+        utilityService.writeCsvFile(dataList,xlsHead, "curie-cna.csv");
+
+        return dataList;
+    }
 
 
 
