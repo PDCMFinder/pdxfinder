67d23e99a1ad90e947ff238b2b226dd47f4296f0
csaba <csaba@ebi.ac.uk>
2019-05-14 11:06:06 +0100

    Extract the DOG from UPDOG

--- indexer/src/main/java/org/pdxfinder/commands/dataloaders/UniversalLoader.java
+++ indexer/src/main/java/org/pdxfinder/commands/dataloaders/UniversalLoader.java
@@ -1,12 +1,5 @@
 package org.pdxfinder.commands.dataloaders;
 
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
-
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.neo4j.ogm.session.Session;
@@ -24,13 +17,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.*;
@@ -49,16 +41,9 @@ import org.apache.poi.ss.usermodel.*;
  *
  * We should call it UPDOG. And it sets up a good joke. Any newcomer says "What's UPDOG?" we can say "Nothing much, what's up with you?"
  */
-public class UniversalLoader extends UniversalLoaderOmic implements CommandLineRunner, ApplicationContextAware {
+public class UniversalLoader extends UniversalLoaderOmic {
 
     private final static Logger log = LoggerFactory.getLogger(UniversalLoader.class);
-
-    Logger logger = LoggerFactory.getLogger(UniversalLoader.class);
-
-    private Options options;
-    private CommandLineParser parser;
-    private CommandLine cmd;
-    private HelpFormatter formatter;
 
     private DataImportService dataImportService;
     private Session session;
@@ -128,91 +113,19 @@ public class UniversalLoader extends UniversalLoaderOmic implements CommandLineR
     @Autowired
     private UtilityService utilityService;
 
-
-    @PostConstruct
-    public void init() {
-        formatter = new HelpFormatter();
-    }
-
-    public UniversalLoader(DataImportService dataImportService) {
+    public UniversalLoader(DataImportService dataImportService, ReportManager reportManager) {
         this.dataImportService = dataImportService;
+        this.reportManager = reportManager;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
 
-        OptionParser parser = new OptionParser();
-        parser.allowsUnrecognizedOptions();
-        parser.accepts("loadUniversal", "Running universal loader (UPDOG)");
-        parser.accepts("loadALL", "Load all, running universal loader (UPDOG)");
-        OptionSet options = parser.parse(args);
+    public void initTemplate(String templateFileStr) throws Exception{
 
-        if (options.has("loadUniversal") || options.has("loadALL")) {
+        FileInputStream excelFile = new FileInputStream(new File(templateFileStr));
+        Workbook workbook = new XSSFWorkbook(excelFile);
+        log.info("Loading template from " + templateFileStr);
+        initializeTemplateData(workbook);
 
-            reportManager = (ReportManager) context.getBean("ReportManager");
-
-
-            File folder = new File(dataRootDir + "UPDOG/");
-
-            if (folder.exists()) {
-
-                File[] updogDirs = folder.listFiles();
-
-                if (updogDirs.length == 0) {
-
-                    log.warn("No subdirs found for the universal loader, skipping");
-                } else {
-
-                    for (int i = 0; i < updogDirs.length; i++) {
-
-                        if (updogDirs[i].isDirectory()) {
-
-                            String templateFileStr = dataRootDir + "UPDOG/" + updogDirs[i].getName() + "/template.xlsx";
-
-                            File template = new File(templateFileStr);
-
-                            //found the template, load it
-                            if (template.exists()) {
-
-                                log.info("******************************************************");
-                                log.info("* Starting universal loader                          *");
-                                log.info("******************************************************");
-
-
-                                FileInputStream excelFile = new FileInputStream(new File(templateFileStr));
-
-                                Workbook workbook = new XSSFWorkbook(excelFile);
-                                log.info("Loading template from " + templateFileStr);
-
-                                initializeTemplateData(workbook);
-
-                                loadTemplateData();
-
-                                workbook.close();
-                                excelFile.close();
-
-                                log.info("******************************************************");
-                                log.info("* Finished running universal loader                  *");
-                                log.info("******************************************************");
-
-                            } else {
-
-                                log.error("No template file found for universal loader in " + updogDirs[i]);
-                            }
-
-                        }
-                    }
-                }
-
-            }
-            //NO UNIVERSAL TEMPLATES, SKIP
-            else {
-
-                log.warn("No UPDOG directory found. Who let the dog out?");
-            }
-
-
-        }
     }
 
 
@@ -301,18 +214,21 @@ public class UniversalLoader extends UniversalLoaderOmic implements CommandLineR
             //check if there is some data in the row and they are not all nulls
             if (dataRow.size() > 0 && !isRowOfNulls(dataRow)) {
 
-               sheetData.add(dataRow);
-                
+                sheetData.add(dataRow);
+
 
             }
 
         }
     }
 
+
+
+
     /**
      * Loads the data from the lists into the DB
      */
-    private void loadTemplateData() {
+    public void loadTemplateData() {
 
         //:: DON'T CHANGE THE ORDER OF THESE METHODS UNLESS YOU WANT TO RISK THE UNIVERSE TO COLLAPSE!
         createDataSourceGroup();
@@ -1390,9 +1306,40 @@ public class UniversalLoader extends UniversalLoaderOmic implements CommandLineR
         return null;
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        context = applicationContext;
+
+    public List<List<String>> getPatientSheetData() {
+        return patientSheetData;
     }
 
+    public List<List<String>> getPatientTumorSheetData() {
+        return patientTumorSheetData;
+    }
+
+    public List<List<String>> getPatientTreatmentSheetData() {
+        return patientTreatmentSheetData;
+    }
+
+    public List<List<String>> getPdxModelSheetData() {
+        return pdxModelSheetData;
+    }
+
+    public List<List<String>> getPdxModelValidationSheetData() {
+        return pdxModelValidationSheetData;
+    }
+
+    public List<List<String>> getDerivedDatasetSheetData() {
+        return derivedDatasetSheetData;
+    }
+
+    public List<List<String>> getSharingAndContactSheetData() {
+        return sharingAndContactSheetData;
+    }
+
+    public List<List<String>> getBreastAndOrColorectalDiagnoSheetData() {
+        return breastAndOrColorectalDiagnoSheetData;
+    }
+
+    public List<List<String>> getLoaderRelatedDataSheetData() {
+        return loaderRelatedDataSheetData;
+    }
 }
