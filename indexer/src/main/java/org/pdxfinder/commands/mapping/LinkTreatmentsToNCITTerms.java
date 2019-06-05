29c5cc3bb00c7f8170e6d6eb7a94595f96133a4a
csaba <csaba@ebi.ac.uk>
2019-06-05 13:35:29 +0100

    Treatment to NCIT command initial commit

---
+++ indexer/src/main/java/org/pdxfinder/commands/mapping/LinkTreatmentsToNCITTerms.java
@@ -0,0 +1,15 @@
+package org.pdxfinder.commands.mapping;
+
+import org.springframework.boot.CommandLineRunner;
+
+/*
+ * Created by csaba on 05/06/2019.
+ */
+public class LinkTreatmentsToNCITTerms implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+    }
+}
