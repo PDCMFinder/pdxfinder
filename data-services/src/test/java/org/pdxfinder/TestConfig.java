74085ad2ef1a9bfc136e4218b62666f58ebe68f1
sprintell <abayomi@sprintell.org>
2019-07-15 10:57:16 +0100

    Admin: Create Embedded in memory mode config for H2 Data Repositories in data-services testConfig

--- data-services/src/test/java/org/pdxfinder/TestConfig.java
+++ data-services/src/test/java/org/pdxfinder/TestConfig.java
@@ -8,18 +8,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 
 /**
  * Test configuration sets up the embedded Neo4J driver in memory mode
+ * And Embedded H2 driver in memory mode
  */
-
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(value = "org.pdxfinder")
 @EnableNeo4jRepositories("org.pdxfinder.graph.repositories")
+@EnableJpaRepositories(basePackages = "org.pdxfinder.rdbms.repositories")
 public class TestConfig {
+
 
   @Bean
   public org.neo4j.ogm.config.Configuration getConfiguration() {
@@ -46,6 +54,31 @@ public class TestConfig {
   public Neo4jTransactionManager transactionManager() {
     return new Neo4jTransactionManager(sessionFactory());
   }
+
+
+  @Bean
+  public DataSource dataSource(){
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+    dataSource.setUsername("neo4j");
+    dataSource.setPassword("neo5j");
+    return dataSource;
+  }
+
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+    entityManagerFactoryBean.setDataSource(dataSource());
+    entityManagerFactoryBean.setPackagesToScan("org.pdxfinder.rdbms");
+
+    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+
+    return entityManagerFactoryBean;
+  }
+
 
 }
 
