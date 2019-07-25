36b8fd8e0c040a07d94283745f3ac06d66742c1d
sprintell <abayomi@sprintell.org>
2019-07-25 11:36:01 +0100

    Create Search Filter for the Persisted Mapping Entities

--- data-model/src/main/java/org/pdxfinder/rdbms/repositories/MappingEntityRepository.java
+++ data-model/src/main/java/org/pdxfinder/rdbms/repositories/MappingEntityRepository.java
@@ -1,19 +1,45 @@
 package org.pdxfinder.rdbms.repositories;
 
 import org.pdxfinder.rdbms.dao.MappingEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
+
+/**
+ * Created by abayomi on 25/07/2019.
+ */
 @Repository
 public interface MappingEntityRepository extends JpaRepository<MappingEntity, Long> {
 
 
     MappingEntity findByMappingKey(String mappingKey);
 
-
     List<MappingEntity> findByMappedTermLabel(String mappedTermLabel);
+
+
+    @Query(value = "Select distinct me from MappingEntity me JOIN me.mappingValues mv " +
+            "WHERE ((lower(me.entityType) = lower(:entityType)) OR :entityType = '') "+
+            "AND ((KEY(mv) = :mappingLabel AND mv = :mappingValue) OR  :mappingValue = '') "
+    )
+    Page<MappingEntity> findByMultipleFilters(@Param("entityType") String entityType,
+
+                                              @Param("mappingLabel") String mappingLabel,
+                                              @Param("mappingValue") String mappingValue,
+
+                                              Pageable pageable);
+
+
+
+    @Query(value = "select me from MappingEntity me JOIN me.mappingValues mv WHERE KEY(mv) = :dataKey AND mv = :dataValue ")
+    List<MappingEntity> findByAttributeAndValue(@Param("dataKey") String dataKey,
+                                                @Param("dataValue") String dataValue);
+
+
 }
