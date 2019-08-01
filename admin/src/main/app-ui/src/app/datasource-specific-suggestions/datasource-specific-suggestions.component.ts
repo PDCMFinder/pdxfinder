7c371903554cc720324059bcb08bf61924e253e5
sprintell <abayomi@sprintell.org>
2019-08-01 21:34:16 +0100

    Implement dynamic column for suggestion table

--- admin/src/main/app-ui/src/app/datasource-specific-suggestions/datasource-specific-suggestions.component.ts
+++ admin/src/main/app-ui/src/app/datasource-specific-suggestions/datasource-specific-suggestions.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {MappingService} from "../mapping.service";
+import {enterView} from "@angular/core/src/render3/instructions";
+import {Mapping} from "../mapping-interface";
 
 @Component({
-  selector: 'app-datasource-specific-suggestions',
-  templateUrl: './datasource-specific-suggestions.component.html',
-  styles: [``]
+    selector: 'app-datasource-specific-suggestions',
+    templateUrl: './datasource-specific-suggestions.component.html',
+    styles: [``]
 })
 export class DatasourceSpecificSuggestionsComponent implements OnInit {
 
-    public entityId = [];
-    public selectedEntity = [];
+    public entityId;
+    public selectedEntity = {};
+
+    public dataLabels;
+    public columnHeaders = [];
 
     public data = {
         DataSource: "",
@@ -19,45 +24,47 @@ export class DatasourceSpecificSuggestionsComponent implements OnInit {
         OriginTissue : ""
     }
 
-
-
     constructor(private router: Router,
                 private route: ActivatedRoute,
                 private _mappingService: MappingService) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-      // From the current url snapshot, get the source parameter and assign to the dataSource property
-      this.route.params.subscribe(
-          params => {
+        // From the current url snapshot, get the source parameter and assign to the dataSource property
+        this.route.params.subscribe(
+            params => {
 
-              this.entityId = params['id'];
+                this.entityId = params['id'];
 
-              // Retrieve the details of Mapping node with this entityId:
-              this._mappingService.getUnmappedDiagnosis()
-                  .subscribe(
-                      data => {
+                // Retrieve the details of Mapping node with this entityId:
+                this._mappingService.getMappingEntityById(this.entityId)
+                    .subscribe(
+                        data => {
 
-                          let myData = data["mappings"]; // This receives the mappings node of the json in required format
-
-                          var count:number = 0;
-                          for (var i of myData) {
-
-                              if (myData[count].entityId.toString() === this.entityId.toString() ){
-                                  this.selectedEntity = myData[count];
-                                  this.data = myData[count].mappingValues;
-                              } count++;
-                          }
-                      }
-                  );
+                            this.selectedEntity = data;
+                            this.data = data.mappingValues;
 
 
+                            // Transfer mappingLabel for this entityType to the template
+                            this.dataLabels = data.mappingLabels;
 
+                            // Convert mapping Labels from CamelCase to Normal Case for Column Headers in Template
+                            this.columnHeaders = [];
+                            this.dataLabels.forEach((mappingLabel) => {
+                                this.columnHeaders.push(mappingLabel.replace(/([a-z])([A-Z])/g, '$1 $2'));
+                            });
+                        }
+                    );
 
+                this.sendDataToParent(this.entityId);
+            }
+        )
+    }
 
-          }
-      )
-  }
+    sendDataToParent(data){
+        this._mappingService.stringDataBus(data);
+    }
+
 
     onSuggestionSubmit(suggestion){
         this._mappingService.componentsDataBus(suggestion);
