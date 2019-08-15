e068e3a602fa91082c045970d1d9ae3a41afb1eb
sprintell <abayomi@sprintell.org>
2019-08-15 11:25:27 +0100

    Add components for archive and skipped terms interface

---
+++ admin/src/main/app-ui/src/app/curation-orphan/curation-orphan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curation-orphan',
+  templateUrl: './curation-orphan.component.html',
+  styles: [``]
+})
+export class CurationOrphanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
