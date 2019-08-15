4de1485f28b9389c8f875dccea786ad960c4a04e
Ross Thorne <rthorne@ebi.ac.uk>
2019-08-15 17:21:11 +0100

    wip tests

--- indexer/src/test/java/org.pdxfinder/commands/dataloaders/LoadIRCCTest.java
+++ indexer/src/test/java/org.pdxfinder/commands/dataloaders/LoadIRCCTest.java
@@ -1,5 +1,38 @@
-import static org.junit.Assert.*;
+package org.pdxfinder.commands.dataloaders;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.pdxfinder.graph.dao.ModelCreation;
+import org.pdxfinder.services.DataImportService;
+import org.pdxfinder.services.dto.LoaderDTO;
+
 
 public class LoadIRCCTest {
 
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this.getClass());
+    }
+
+    @Mock
+    LoadIRCC loader;
+
+    @InjectMocks
+    LoaderDTO dto;
+
+    @InjectMocks
+    ModelCreation modelCreation;
+
+    @InjectMocks
+    DataImportService dataImportService;
+
+    @Test
+    public void addAccessModality() {
+        dto.setModelCreation(modelCreation);
+        loader.addAccessModality();
+    }
 }
