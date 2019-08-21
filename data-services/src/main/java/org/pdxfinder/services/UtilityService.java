3844ebf96152ae902f7163f83dafea547a4c83d4
sprintell <abayomi@sprintell.org>
2019-08-21 10:46:30 +0100

    Add Camel Case converters to utilties

--- data-services/src/main/java/org/pdxfinder/services/UtilityService.java
+++ data-services/src/main/java/org/pdxfinder/services/UtilityService.java
@@ -25,6 +25,7 @@ import java.util.*;
 import java.util.stream.Stream;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 /*
@@ -529,7 +530,10 @@ public class UtilityService {
                     Map<String, String> rowMap = new LinkedHashMap<>();
                     for (String columnHead : tableHead) {
 
-                        rowMap.put(columnHead.trim(), rowDataArr[column].trim());
+                        String dKey = columnHead.trim().replaceAll("\"","");
+                        String dValue = rowDataArr[column].trim().replaceAll("\"","");
+
+                        rowMap.put(dKey, dValue);
                         column++;
                     }
                     csvMap.add(rowMap);
@@ -825,7 +829,7 @@ public class UtilityService {
     }
 
 
-    // FILE WRITE: WRITE STRIN DATA TO FILE
+    // FILE WRITE: WRITE STRING DATA TO FILE
     public void writeToFile(String data, String destination, Boolean shouldAppend){
 
         // Write to the file using BufferedReader and FileWriter
@@ -905,6 +909,25 @@ public class UtilityService {
     /*************************************************************************************************************
      *                                          OTHER UTILITIES                                                  *
      ************************************************************************************************************/
+
+    public String camelCaseToSentence(String s) {
+        return s.replaceAll(
+                String.format("%s|%s|%s",
+                              "(?<=[A-Z])(?=[A-Z][a-z])",
+                              "(?<=[^A-Z])(?=[A-Z])",
+                              "(?<=[A-Za-z])(?=[^A-Za-z])"
+                ),
+                " "
+        );
+    }
+
+
+    public String sentenceToCamelCase(String input) {
+
+        String output = StringUtils.capitalize(input.split("-")[0]) + StringUtils.capitalize(input.split("-")[1]);
+
+        return output;
+    }
 
 
     public String splitText(String data, String delim, String seperator){
