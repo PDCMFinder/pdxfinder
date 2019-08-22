7a52bffa7b3cd7ba8b8e4f725a7bb7c682d2b043
jmason-ebi <jmason@ebi.ac.uk>
2019-08-22 13:42:27 +0100

    Fix unit test

--- indexer/src/test/java/org.pdxfinder/commands/TestConfig.java
+++ indexer/src/test/java/org.pdxfinder/commands/TestConfig.java
@@ -1,8 +1,6 @@
 package org.pdxfinder.commands;
 
-
 import org.neo4j.ogm.session.SessionFactory;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,95 +12,25 @@ import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 
 import javax.sql.DataSource;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 
 
 /**
  * Test configuration sets up the embedded Neo4J driver in memory mode
- * And Embedded H2 driver in memory mode
  */
 @Configuration
-//@EnableTransactionManagement
-@ComponentScan(value = "org.pdxfinder")
-//@EnableNeo4jRepositories("org.pdxfinder.graph.repositories")
-//@EnableJpaRepositories(basePackages = "org.pdxfinder.rdbms.repositories")
-@EntityScan("org.pdxfinder")
+@EnableTransactionManagement
+@EnableNeo4jRepositories("org.pdxfinder.graph.repositories")
 public class TestConfig {
 
+    @Bean
+    public SessionFactory sessionFactory() { return new SessionFactory("org.pdxfinder"); }
 
-  @Bean
-  public org.neo4j.ogm.config.Configuration getConfiguration() {
-    org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
-
-//     To persist the database, uncomment this section
-    String pathToDb = Paths.get(".").toAbsolutePath().normalize().toString() + "/target/test_graph.db";
-    config
-            .driverConfiguration()
-            .setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver")
-            .setURI("file://" + pathToDb);
-
-    System.out.println(config);
-
-    return config;
-  }
-
-  @Bean
-  public SessionFactory sessionFactory() {
-    return new SessionFactory(getConfiguration(), "org.pdxfinder");
-  }
-
-  @Bean
-  public Neo4jTransactionManager transactionManager() {
-    return new Neo4jTransactionManager(sessionFactory());
-  }
-
-
-  @Bean
-  public DataSource dataSource(){
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName("org.h2.Driver");
-    dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-    dataSource.setUsername("neo4j");
-    dataSource.setPassword("neo5j");
-    return dataSource;
-  }
-
-
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-    entityManagerFactoryBean.setDataSource(dataSource());
-    entityManagerFactoryBean.setPackagesToScan("org.pdxfinder.rdbms");
-
-    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
-
-    return entityManagerFactoryBean;
-  }
-
-
-  @Bean
-  public SpringTemplateEngine springTemplateEngine() {
-    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-    templateEngine.addTemplateResolver(htmlTemplateResolver());
-    return templateEngine;
-  }
-
-  @Bean
-  public SpringResourceTemplateResolver htmlTemplateResolver(){
-    SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
-    emailTemplateResolver.setPrefix("classpath:/templates/");
-    emailTemplateResolver.setSuffix(".html");
-    //emailTemplateResolver.setTemplateMode();
-    emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
-    return emailTemplateResolver;
-  }
-
+    @Bean
+    public Neo4jTransactionManager transactionManager() {
+        return new Neo4jTransactionManager(sessionFactory());
+    }
 
 }
 
--- indexer/src/test/java/org.pdxfinder/commands/dataloaders/LoadIRCCTest.java
+++ indexer/src/test/java/org.pdxfinder/commands/dataloaders/LoadIRCCTest.java
@@ -1,49 +1,57 @@
 package org.pdxfinder.commands.dataloaders;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-
-
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.pdxfinder.commands.BaseTest;
+import org.pdxfinder.graph.dao.Group;
 import org.pdxfinder.graph.dao.ModelCreation;
-import org.pdxfinder.services.dto.LoaderDTO;
+import org.pdxfinder.services.DataImportService;
+import org.pdxfinder.services.UtilityService;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.List;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
 public class LoadIRCCTest extends BaseTest {
 
-//    @Before
-//    public void setUp() throws Exception {
-//        MockitoAnnotations.initMocks(this.getClass());
-//    }
+    private Group g;
+    private Group badG;
 
-//    @Before
-//    public void init(){
-//        MockitoAnnotations.initMocks(LoadIRCC.class);
-//    }
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
 
-    @Mock
-    private List list;
+        g = new Group();
+        g.setName("transnational access");
 
-    @Mock
-    private LoaderDTO dto;
+        badG = new Group();
+        badG.setName("another group");
 
-    @Mock
-    private ModelCreation modelCreation;
+    }
 
-//    @Autowired
-//    void LoadIIRCTest(LoadIRCC loader) {
-//        this.loader = loader;
-//    }
+    @MockBean
+    UtilityService utilityService;
 
-//    @Spy
-//    @InjectMocks
+    @MockBean
+    DataImportService dataImportService;
+
+    @InjectMocks
     private LoadIRCC loader;
 
     @Test
     public void addAccessModality() {
-        dto.setModelCreation(modelCreation);
+
+        when(dataImportService.getAccessibilityGroup("", "transnational access")).thenReturn(g);
+
+        ModelCreation mc = new ModelCreation();
+        loader.dto.setModelCreation(mc);
+
         loader.addAccessModality();
+
+        assertThat(loader.dto.getModelCreation().getGroups().contains(g), is(true));
+        assertThat(loader.dto.getModelCreation().getGroups().contains(badG), is(false));
     }
 }
