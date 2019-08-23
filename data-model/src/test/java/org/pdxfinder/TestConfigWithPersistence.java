ed61766fb67c31e99cd31d6b970798c67b00680f
afollette <follette.alex@gmail.com>
2019-08-23 15:19:01 +0100

    Create load test on DataProjection
    Test Ignored by default. Created new tests configurations
    and base test for separation configurations for persistence.

--- data-model/src/test/java/org/pdxfinder/TestConfigWithPersistence.java
+++ data-model/src/test/java/org/pdxfinder/TestConfigWithPersistence.java
@@ -8,18 +8,28 @@ import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/**
- * Test configuration sets up the embedded Neo4J driver in memory mode
- */
+import java.nio.file.Paths;
+
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(value = "org.pdxfinder")
 @EnableNeo4jRepositories("org.pdxfinder.graph.repositories")
-public class TestConfig{
+public class TestConfigWithPersistence {
 
     @Bean
     public org.neo4j.ogm.config.Configuration getConfiguration() {
-        return new org.neo4j.ogm.config.Configuration();
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+
+        //   To persist the database, uncomment this section
+        String pathToDb = Paths.get(".").toAbsolutePath().normalize().toString() + "/target/test_graph.db";
+        config
+                .driverConfiguration()
+                .setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver")
+                .setURI("file://" + pathToDb);
+
+        System.out.println(config);
+
+        return config;
     }
 
     @Bean
@@ -31,4 +41,7 @@ public class TestConfig{
     public Neo4jTransactionManager transactionManager() {
         return new Neo4jTransactionManager(sessionFactory());
     }
+
 }
+
+
---
+++ data-model/src/test/java/org/pdxfinder/graph/dao/DataProjectionsTimeTest.java
@@ -0,0 +1,90 @@
+package org.pdxfinder.graph.dao;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.pdxfinder.BaseTestWithPersistence;
+import org.pdxfinder.graph.repositories.DataProjectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class DataProjectionsTimeTest extends BaseTestWithPersistence {
+
+    private static final String LABEL = "TEST";
+
+    @Autowired
+    private DataProjectionRepository dataProjectionRepository;
+
+    @Ignore
+    @Test
+    public void Given_DataProjection_When_CascadeCreateSaveAndFinds_Then_PrintTimes(){
+
+        byte[] tenKiB = generateRandomByte(4);
+        byte[] hundredKib = generateRandomByte(5);
+        byte[] oneMib = generateRandomByte(6);
+        byte[] tenMiB = generateRandomByte(7);
+        byte[] hundredMiB = generateRandomByte(8);
+        List<Long> elapsedTime = timeCreateSaveAndFindAndReturnElapsedTime(tenKiB);
+        List<Long> elapsedTime1 = timeCreateSaveAndFindAndReturnElapsedTime(hundredKib);
+        List<Long> elapsedTime2 = timeCreateSaveAndFindAndReturnElapsedTime(oneMib);
+        List<Long> elapsedTime3 = timeCreateSaveAndFindAndReturnElapsedTime(tenMiB);
+        //long elapsedTime4 = timeCreateSaveAndFindAndReturnElapsedTime(hundredMiB);
+
+        StringBuilder timeTable = new StringBuilder(
+                String.format(" Data Size: %s Time %s %s %s Average %s \n", tenKiB.length, elapsedTime.get(0), elapsedTime.get(1), elapsedTime.get(2), elapsedTime.get(3)));
+        timeTable.append(
+                String.format(" Data Size: %s Time %s %s %s Average %s \n", hundredKib.length, elapsedTime1.get(0), elapsedTime1.get(1), elapsedTime1.get(2), elapsedTime1.get(3)));
+        timeTable.append(
+                String.format(" Data Size: %s Time %s %s %s  Average %s \n", oneMib.length, elapsedTime2.get(0), elapsedTime2.get(1), elapsedTime2.get(2), elapsedTime2.get(3)));
+        timeTable.append(
+                String.format(" Data Size: %s Time %s %s %s Average %s \n", tenMiB.length, elapsedTime3.get(0), elapsedTime3.get(1), elapsedTime3.get(2), elapsedTime3.get(3)));
+        //timeTable.append(
+          //      String.format(" Data Size: %s Time %s \n", hundredMiB.length, elapsedTime4));
+
+        System.out.println(timeTable);
+    }
+
+
+    private byte[] generateRandomByte(int exponent){
+
+        int byteSize = (int) Math.pow(10, exponent);
+        byte[] generatedByte = new byte[byteSize];
+        new Random().nextBytes(generatedByte);
+        return generatedByte;
+    }
+
+    private List<Long> timeCreateSaveAndFindAndReturnElapsedTime(byte[] randomData){
+
+        List<Long> time = new ArrayList<>();
+        long average;
+        long start;
+        long end;
+
+        DataProjection returnProjection;
+
+        for(int i = 0; i < 3; i++) {
+
+            start = System.currentTimeMillis();
+
+            DataProjection dataProjection = new DataProjection();
+            dataProjection.setLabel(LABEL);
+            dataProjection.setValue(Arrays.toString(randomData));
+
+            dataProjectionRepository.save(dataProjection);
+            returnProjection = dataProjectionRepository.findByLabel(LABEL);
+            Assert.assertEquals(LABEL, returnProjection.getLabel());
+
+            end = System.currentTimeMillis();
+
+            dataProjectionRepository.deleteAll();
+            time.add(end - start);
+        }
+        average = ((time.get(0)+time.get(1)+time.get(2))/3);
+        time.add(average);
+        return time;
+    }
+}
