f3d53b0f9156a8e02c1968b8ef071177b97bad8f
sprintell <abayomi@sprintell.org>
2019-09-12 17:16:35 +0100

    Add caching to speed up ontology suggestions

--- data-services/src/main/java/org/pdxfinder/services/MappingService.java
+++ data-services/src/main/java/org/pdxfinder/services/MappingService.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -992,6 +993,7 @@ public class MappingService {
     }
 
 
+    @Cacheable("ontologies")
     public List<OntologyTerm> getOntologyTermsByType(String type) {
 
         return ontologyTermRepository.findByType(type);
