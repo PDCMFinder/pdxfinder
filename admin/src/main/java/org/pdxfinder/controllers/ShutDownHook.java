4f988b88390ecc063ed1e2504c320e2f1d94af58
sprintell <abayomi@sprintell.org>
2019-09-23 15:54:26 +0100

    Add shut down hook for properly shutting down the admin app

---
+++ admin/src/main/java/org/pdxfinder/controllers/ShutDownHook.java
@@ -0,0 +1,26 @@
+package org.pdxfinder.controllers;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShutDownHook implements ApplicationContextAware {
+
+    private ApplicationContext context;
+
+    @PostMapping("/shutdown")
+    public void shutdownContext() {
+        ((ConfigurableApplicationContext) context).close();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        this.context = ctx;
+
+    }
+}
