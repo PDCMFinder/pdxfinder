11911d5edb323485b9d005d6f87447ec3b2528bf
sprintell <abayomi@sprintell.org>
2019-10-01 15:01:56 +0100

    Remove sql comments

--- data-model/src/main/java/org/pdxfinder/rdbms/repositories/MappingEntityRepository.java
+++ data-model/src/main/java/org/pdxfinder/rdbms/repositories/MappingEntityRepository.java
@@ -62,19 +62,7 @@ public interface MappingEntityRepository extends JpaRepository<MappingEntity, Lo
 
 
 
-    /*
-    Query:
-    SELECT DISTINCT lower(MAPPING_VALUES),
-                    COUNT(CASE WHEN STATUS = 'unmapped' THEN 1 END) AS UNMAPPED,
-                    COUNT(CASE WHEN STATUS <> 'unmapped' AND STATUS <> 'orphaned' THEN 1 END) AS MAPPED,
-                    COUNT(CASE WHEN STATUS = 'validated' THEN 1 END) AS VALIDATED,
-                    COUNT(CASE WHEN STATUS = 'created' THEN 1 END) AS UNVALIDATED,
-                    COUNT(CASE WHEN STATUS = 'orphaned' THEN 1 END) AS ORPHANED
 
-    FROM MAPPING_ENTITY me JOIN MAPPING_VALUES mv on me.ENTITY_ID = mv.MAPPING_ENTITY_ID
-    WHERE mv.MAPPING_VALUES_KEY = 'DataSource' AND ENTITY_TYPE='treatment'  GROUP BY lower(MAPPING_VALUES) ORDER BY UNMAPPED desc
-
-     */
     @Query("SELECT distinct lower(mv), " +
             " count(case when me.status = 'unmapped' THEN 1 END) as unmapped, " +
             " count(case when me.status <> 'unmapped' AND me.status <> 'orphaned'  THEN 1 END) as mapped, " +
