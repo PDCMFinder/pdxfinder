58c7cea10875c441bb2fc31246fd071a0752c894
csaba <csaba@ebi.ac.uk>
2019-11-12 13:21:09 +0000

    Add tests

--- indexer/src/test/java/org/pdxfinder/dataexport/DataExportTest.java
+++ indexer/src/test/java/org/pdxfinder/dataexport/DataExportTest.java
@@ -3,21 +3,12 @@ package org.pdxfinder.dataexport;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.pdxfinder.BaseTest;
 import org.pdxfinder.graph.dao.*;
-import org.pdxfinder.graph.repositories.*;
 import org.pdxfinder.services.DataImportService;
 import org.pdxfinder.services.UtilityService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
@@ -87,16 +78,78 @@ public class DataExportTest extends BaseTest {
         ModelCreation modelCreation = new ModelCreation();
         modelCreation.setSourcePdxId("m123");
 
-
         when(dataImportService.findPatientTumorAtCollectionDataByDS(providerGroup)).thenReturn(patientList);
-        //when(dataImportService.findModelBySample(sample).getSourcePdxId()).thenReturn(modelCreation.getSourcePdxId());
 
         universalDataExporter.init("", providerGroup);
 
         List<List<String>> patientTumorAtCollection = universalDataExporter.getPatientTumorSheetData();
         Assert.assertEquals("p123", patientTumorAtCollection.get(0).get(0));
+    }
+
+    @Test
+    public void TestPdxModelDetailsAndValidations(){
+
+        ModelCreation model = new ModelCreation();
+        model.setSourcePdxId("m123");
+        HostStrain hostStrain = new HostStrain("HostSymbol", "HostName");
+
+        Specimen specimen = new Specimen();
+        specimen.setPassage("1");
+        specimen.setHostStrain(hostStrain);
+        model.addSpecimen(specimen);
+
+        QualityAssurance qualityAssurance = new QualityAssurance("technology", "description", "1,2");
+        model.addQualityAssurance(qualityAssurance);
+
+        List<ModelCreation> modelCreationList = new ArrayList<>();
+        modelCreationList.add(model);
+
+        when( dataImportService.findModelsWithSpecimensAndQAByDS(providerGroup.getAbbreviation())).thenReturn(modelCreationList);
+
+        universalDataExporter.init("", providerGroup);
+        List<List<String>> pdxModelDetails = universalDataExporter.getPdxModelSheetData();
+        List<List<String>> pdxModelValidations = universalDataExporter.getPdxModelValidationSheetData();
+
+
+        Assert.assertEquals("m123", pdxModelDetails.get(0).get(0));
+        Assert.assertEquals("HostName", pdxModelDetails.get(0).get(1));
+
+        Assert.assertEquals("m123", pdxModelValidations.get(0).get(0));
+        Assert.assertEquals("technology", pdxModelValidations.get(0).get(1));
 
     }
+
+    @Test
+    public void TestSharingAndContact(){
+
+        ModelCreation model = new ModelCreation();
+        model.setSourcePdxId("m123");
+
+        Group accessGroup = new Group("Academia", "transnational");
+        Group project = new Group("project1", "p1", "Project");
+
+        model.addGroup(providerGroup);
+        model.addGroup(accessGroup);
+        model.addGroup(project);
+
+        ExternalUrl url = new ExternalUrl(ExternalUrl.Type.CONTACT,"email@address.com");
+        List<ExternalUrl> urlList = new ArrayList<>();
+        urlList.add(url);
+        model.setExternalUrls(urlList);
+
+        List<ModelCreation> modelCreationList = new ArrayList<>();
+        modelCreationList.add(model);
+
+        when(  dataImportService.findModelsWithSharingAndContactByDS(providerGroup.getAbbreviation())).thenReturn(modelCreationList);
+
+        universalDataExporter.init("", providerGroup);
+        List<List<String>> sharingAndContact = universalDataExporter.getSharingAndContactSheetData();
+
+        Assert.assertEquals("m123", sharingAndContact.get(0).get(0));
+        Assert.assertEquals("Academia", sharingAndContact.get(0).get(1));
+
+    }
+
 
 
 }
