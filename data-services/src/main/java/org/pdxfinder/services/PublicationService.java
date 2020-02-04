5d4a671d33a03fb63e27052e69149a2eeb4a34ae
sprintell <abayomi@sprintell.org>
2020-02-04 13:30:37 +0000

    Added publication service

---
+++ data-services/src/main/java/org/pdxfinder/services/PublicationService.java
@@ -0,0 +1,63 @@
+package org.pdxfinder.services;
+
+import org.pdxfinder.services.constants.DataUrl;
+import org.pdxfinder.services.europepmc.Publication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PublicationService {
+
+    private RestTemplate restTemplate;
+    private static final String PUBLICATION_PREFIX = "PMID:";
+    private static final String EMPTY_STRING = "";
+
+    public PublicationService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public List<Publication> getEuropePmcPublications(List<String> pubMedIds) {
+
+        pubMedIds = sanitizePubMedIds(pubMedIds);
+
+        List<Publication> publications = new ArrayList<>();
+
+        pubMedIds.forEach(pubMedId->{
+
+            String api = String.format("%s?query=%s&format=json", DataUrl.EUROPE_PMC_URL.get(), pubMedId);
+
+            publications.add(restTemplate.getForObject(api, Publication.class));
+        });
+
+
+        return publications;
+    }
+
+
+    public List<String> sanitizePubMedIds(List<String> pubMedIds){
+
+        // turn colon separated into list:
+        List<String> cleanedPubMedIds = new ArrayList<>();
+
+        pubMedIds.forEach(pubMedId -> {
+
+            // Remove all spaces:
+            pubMedId = pubMedId.replace(PUBLICATION_PREFIX, EMPTY_STRING);
+            pubMedId = pubMedId.replaceAll("\\s+",EMPTY_STRING);
+
+            if (pubMedId.contains(";")){
+                cleanedPubMedIds.addAll(Arrays.asList(pubMedId.split(";")));
+            }else {
+                cleanedPubMedIds.add(pubMedId);
+            }
+
+        });
+
+        return cleanedPubMedIds;
+    }
+}
