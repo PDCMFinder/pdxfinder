55adb5ac30f976e49ea1ba1a24b631cbcbe7e5dd
abayomi <abayomi@sprintell.org>
2020-04-16 15:04:00 +0100

    Add feature to automate refreshing of embeddedDB from developer's machine and copy cache directory if it exists

--- indexer/src/main/java/org/pdxfinder/DatasourceConfig.java
+++ indexer/src/main/java/org/pdxfinder/DatasourceConfig.java
@@ -1,6 +1,9 @@
 package org.pdxfinder;
 
+import org.apache.commons.io.FileUtils;
 import org.neo4j.ogm.session.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
@@ -19,16 +22,16 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-
-/*
- * Created by abayomi on 12/02/2019.
- */
+import java.io.File;
+import java.io.IOException;
 
 @Configuration
 @EnableNeo4jRepositories(basePackages = "org.pdxfinder.graph")
 @EnableJpaRepositories(basePackages = "org.pdxfinder.rdbms", transactionManagerRef = "h2TransactionManager")
 @EnableTransactionManagement
 public class DatasourceConfig {
+
+  private Logger log = LoggerFactory.getLogger(DatasourceConfig.class);
 
   /*************************************************************************************************************
    *     NEO4J GRAPH DATABASE CONFIGURATION           *
@@ -37,11 +40,18 @@ public class DatasourceConfig {
   @Value("${spring.data.neo4j.uri}")
   private String embeddedDataDir;
 
+  @Value("${db-cache-dir}")
+  private String cacheDataDir;
+
+  @Value("${db-refresh}")
+  private boolean embeddedDbRefresh;
+
   @Primary
   @Bean
-  public SessionFactory sessionFactory() {
-    org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+  public SessionFactory sessionFactory() throws IOException {
 
+    this.refreshEmbeddedDBFromCache();
+    org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
     config
             .driverConfiguration()
             .setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver")
@@ -53,7 +63,7 @@ public class DatasourceConfig {
 
   @Primary
   @Bean(name = "neo4jTransactionManager")
-  public Neo4jTransactionManager transactionManager() {
+  public Neo4jTransactionManager transactionManager() throws IOException {
     return new Neo4jTransactionManager(sessionFactory());
   }
 
@@ -94,6 +104,28 @@ public class DatasourceConfig {
             h2TransactionManager,
             neo4jTransactionManager
     );
+  }
+
+
+  private void refreshEmbeddedDBFromCache() throws IOException{
+
+    boolean dataDeleted = false;
+    String dataDir = embeddedDataDir.replace("file://", "");
+
+    if (embeddedDbRefresh) {
+      File data = new File(dataDir);
+      File cache = new File(cacheDataDir);
+
+      if (data.isDirectory()){
+        FileUtils.cleanDirectory(data);
+        dataDeleted = true;
+      }
+
+      if (cache.isDirectory() && dataDeleted){
+        FileUtils.copyDirectory(cache, data);
+      }
+
+    }
   }
 
 
