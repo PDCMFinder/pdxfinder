a0b77a6cdd099ab2218ab405a719f604407c70a1
abayomi <abayomi@sprintell.org>
2020-05-28 13:15:14 +0100

    Added restTemplate Bean

--- web/src/main/java/org/pdxfinder/Application.java
+++ web/src/main/java/org/pdxfinder/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfigurat
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching
@@ -14,5 +16,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
