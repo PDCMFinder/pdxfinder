6fae884a6280b85be18f24a7af488a6a0e6264c0
csaba <csaba@ebi.ac.uk>
2020-05-28 20:41:45 +0100

    Autowire RestTemplate

--- data-services/src/main/java/org/pdxfinder/services/PublicationService.java
+++ data-services/src/main/java/org/pdxfinder/services/PublicationService.java
@@ -4,6 +4,7 @@ import org.pdxfinder.services.constants.DataUrl;
 import org.pdxfinder.services.dto.europepmc.Publication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,11 +15,13 @@ import java.util.List;
 @Service
 public class PublicationService {
 
+    @Autowired
     private RestTemplate restTemplate;
 
     private static final String PUBLICATION_PREFIX = "PMID:";
     private static final String EMPTY_STRING = "";
 
+    @Autowired
     public PublicationService(RestTemplate restTemplate) {
       this.restTemplate = restTemplate;
     }
