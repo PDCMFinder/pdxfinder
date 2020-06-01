4b06123948ede75796fcf925b7eb15d8bd196f2f
sprintell <abayomi@sprintell.org>
2020-06-01 19:30:23 +0100

    Add back end service to retrieve reference data

---
+++ data-services/src/main/java/org/pdxfinder/services/ReferenceDbService.java
@@ -0,0 +1,77 @@
+package org.pdxfinder.services;
+
+import org.pdxfinder.graph.dao.MolecularData;
+import org.pdxfinder.services.constants.DataUrl;
+import org.pdxfinder.services.dto.pdxgun.MarkerData;
+import org.pdxfinder.services.dto.pdxgun.ReferenceData;
+import org.pdxfinder.services.graphqlclient.Condition;
+import org.pdxfinder.services.graphqlclient.GraphQlBuilder;
+import org.pdxfinder.services.graphqlclient.Operator;
+import org.pdxfinder.services.graphqlclient.Select;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
+
+@Service
+public class ReferenceDbService {
+
+    private RestTemplate restTemplate;
+
+    public ReferenceDbService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public List<String> getMarkerListFromMolecularData(List<MolecularData> molecularDataList){
+
+        List<String> markerList = new ArrayList<>();
+        molecularDataList.forEach( mData -> markerList.add(mData.getMarker()) );
+
+        return markerList;
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public Map<String, MarkerData> getReferenceDataForMarkerList(List<String> markerList){
+
+        Map<String, Condition> conditions = new LinkedHashMap<>();
+        conditions.put("name", new Condition().setOperator(Operator.IN).setValue(markerList));
+
+        Select select = new Select()
+                .columns(Arrays.asList("name", "url", "resource{name}"))
+                .table("gene")
+                .conditions(conditions);
+        String graphQlQuery = GraphQlBuilder.selectQuery(select);
+
+        HttpEntity<Object> request = new HttpEntity<>(graphQlQuery);
+        ReferenceData referenceData = restTemplate.postForObject(DataUrl.K8_SERVICE_URL.get(), request, ReferenceData.class);
+
+        Map<String, Map<String, String>> dataMap = new HashMap<>();
+        referenceData.getData().getGene().forEach(gene -> {
+
+            String geneName = gene.getName();
+            String resource = gene.getResource().getName();
+            String url = gene.getUrl();
+
+            if (dataMap.containsKey(geneName)){
+                dataMap.get(geneName).put(resource, url);
+            }else {
+                Map<String, String> map = new HashMap<>();
+                map.put(resource, url);
+                dataMap.put(gene.getName(), map);
+            }
+        });
+
+        Map<String, MarkerData> markerDataMap = new HashMap<>();
+        dataMap.forEach((key, value)->{
+            MarkerData markerData = new MarkerData();
+            markerData.setSymbol(key)
+                    .setRefData(value)
+                    .build();
+            markerDataMap.put(key, markerData);
+        });
+
+        return markerDataMap;
+    }
+}
