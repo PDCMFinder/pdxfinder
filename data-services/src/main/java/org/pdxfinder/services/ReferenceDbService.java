d928ed2e1cd1395196caa8d2a3762e6b6e4b1907
sprintell <abayomi@sprintell.org>
2020-06-11 01:12:44 +0100

    Add feature to generate url and link to open cravat

--- data-services/src/main/java/org/pdxfinder/services/ReferenceDbService.java
+++ data-services/src/main/java/org/pdxfinder/services/ReferenceDbService.java
@@ -1,11 +1,8 @@
 package org.pdxfinder.services;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.pdxfinder.graph.dao.MolecularData;
+import org.apache.commons.lang3.StringUtils;
 import org.pdxfinder.services.constants.DataUrl;
+import org.pdxfinder.services.dto.pdxgun.ApiData;
 import org.pdxfinder.services.dto.pdxgun.Reference;
 import org.pdxfinder.services.dto.pdxgun.ReferenceData;
 import org.pdxfinder.services.graphqlclient.Condition;
@@ -18,14 +15,15 @@ import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.*;
 import java.util.*;
+import java.util.stream.Stream;
 
 @Service
 public class ReferenceDbService {
 
     private static final String COSM_PREFIX = "COSM";
     private static final String COSMIC = "COSMIC";
+    private static final String CRAVAT = "CRAVAT";
     private Logger log = LoggerFactory.getLogger(ReferenceDbService.class);
     private RestTemplate restTemplate;
 
@@ -34,24 +32,14 @@ public class ReferenceDbService {
     }
 
     @SuppressWarnings("WeakerAccess")
-    public List<String> getMarkerListFromMolecularData(List<MolecularData> molecularDataList){
-
-        List<String> markerList = new ArrayList<>();
-        molecularDataList.forEach( mData -> markerList.add(mData.getMarker()) );
-
-        return markerList;
-    }
-
-
-    @SuppressWarnings("WeakerAccess")
-    public Map<String, Reference> getReferenceDataForMarkerList(List<String> markerList){
+    public Map<String, Reference> getReferenceData(List<String> markerList, String dataType){
 
         Map<String, Condition> conditions = new LinkedHashMap<>();
         conditions.put("name", new Condition().setOperator(Operator.IN).setValue(markerList));
 
         Select select = new Select()
                 .columns(Arrays.asList("name", "url", "resource{name}"))
-                .table("gene")
+                .table(dataType)
                 .conditions(conditions);
         String graphQlQuery = GraphQlBuilder.selectQuery(select);
         HttpEntity<Object> request = new HttpEntity<>(graphQlQuery);
@@ -60,29 +48,34 @@ public class ReferenceDbService {
         Map<String, Reference> markerDataMap = new HashMap<>();
         try{
             referenceData = restTemplate.postForObject(DataUrl.K8_SERVICE_URL.get(), request, ReferenceData.class);
-            markerDataMap = clusterReferenceDataByMarker(referenceData);
+            markerDataMap = clusterReferenceDataByMarker(referenceData, dataType);
         }catch (Exception e){
-            log.info("Reference Database could not be retrieved {}", e);
+            log.info("Reference Database could not be retrieved");
         }
         return  markerDataMap;
     }
 
-
-    private Map<String, Reference> clusterReferenceDataByMarker(ReferenceData referenceData){
+    private Map<String, Reference> clusterReferenceDataByMarker(ReferenceData referenceData, String dataType){
 
         Map<String, Map<String, String>> dataMap = new HashMap<>();
-        referenceData.getData().getGene().forEach(gene -> {
 
-            String geneName = gene.getName();
-            String resource = gene.getResource().getName();
-            String url = gene.getUrl();
+        List<ApiData> dData;
+        if (dataType.equals("gene")){
+            dData = referenceData.getData().getGene();
+        }else {
+            dData = referenceData.getData().getVariant();
+        }
+        dData.forEach(data -> {
+            String dataName = data.getName();
+            String resource = data.getResource().getName();
+            String url = data.getUrl();
 
-            if (dataMap.containsKey(geneName)){
-                dataMap.get(geneName).put(resource, url);
+            if (dataMap.containsKey(dataName)){
+                dataMap.get(dataName).put(resource, url);
             }else {
                 Map<String, String> map = new HashMap<>();
                 map.put(resource, url);
-                dataMap.put(gene.getName(), map);
+                dataMap.put(data.getName(), map);
             }
         });
 
@@ -98,21 +91,43 @@ public class ReferenceDbService {
     }
 
     @SuppressWarnings("WeakerAccess")
-    public Reference getVariantTypeReference(String label){
+    public Reference getAminoAcidChangeReference(String aminoAcidChange,
+                                                 Map<String, Reference> referenceDataMap,
+                                                 String variation,
+                                                 String chrom,
+                                                 String seqStart,
+                                                 String refAllele,
+                                                 String altAllele){
 
-        Map<String, Object> dbUrl = new HashMap<>();
+        Map<String, String> referenceDBs = new HashMap<>();
         Reference reference = null;
-        if (Optional.ofNullable(label).isPresent()){
-            if (label.contains(COSM_PREFIX)){
-                String cosmValue = label.split(COSM_PREFIX)[1];
-                dbUrl.put(COSMIC, String.format("%s=%s", DataUrl.COSMIC_URL.get(), cosmValue));
+
+        if (Optional.ofNullable(aminoAcidChange).isPresent()){
+            reference = getReference(aminoAcidChange, referenceDataMap);
+            referenceDBs = Optional.ofNullable(reference.getReferenceDbs()).orElse(new HashMap<>());
+        }
+
+        if (Optional.ofNullable(variation).isPresent()){
+            if (variation.contains(COSM_PREFIX)){
+                String cosmValue = variation.split(COSM_PREFIX)[1].replace(",","");
+                referenceDBs.put(COSMIC, String.format("%s=%s", DataUrl.COSMIC_URL.get(), cosmValue));
             }
-            return new Reference(label).setReferenceDbs(dbUrl);
+
+            if (StringUtils.containsIgnoreCase(variation, "Rs") &&
+                    Stream.of(chrom, seqStart, refAllele, altAllele).allMatch(Objects::nonNull)) {
+                referenceDBs.put(CRAVAT, String.format("%s?chrom=chr%s&pos=%s&ref_base=%s&alt_base=%s",
+                                                       DataUrl.CRAVAT_URL.get(),
+                                                       chrom.replace("chr",""),
+                                                       seqStart,
+                                                       refAllele,
+                                                       altAllele));
+            }
+            return new Reference(aminoAcidChange).setReferenceDbs(referenceDBs);
         }
         return reference;
     }
 
-    public Reference getMarkerReference(String symbol, Map<String, Reference> referenceDataMap){
+    public Reference getReference(String symbol, Map<String, Reference> referenceDataMap){
         Optional<Reference> optionalMarkerData = Optional.ofNullable(referenceDataMap.get(symbol));
         return optionalMarkerData.orElse(new Reference(symbol));
     }
