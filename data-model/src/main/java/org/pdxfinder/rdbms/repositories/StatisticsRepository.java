8d51dc137182fe71a84cd064760c0e531b6e575a
afollette <follette.alex@gmail.com>
2020-10-27 14:42:23 +0000

    deleted data-model

--- data-model/src/main/java/org/pdxfinder/rdbms/repositories/StatisticsRepository.java
+++
@@ -1,9 +0,0 @@
-package org.pdxfinder.rdbms.repositories;
-
-import org.pdxfinder.rdbms.dao.Statistics;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatisticsRepository extends JpaRepository<Statistics, Integer> {
-}
